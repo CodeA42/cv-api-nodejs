247ae59d8a647e8e9e4bc40636dd67255471271f
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-19 13:31:48 +0300

    ; is annoying

--- lib/db/queries/user/get.ts
+++ lib/db/queries/user/get.ts
@@ -1,12 +1,12 @@
-import { AppDataSource } from "../..";
-import User from "../../Entities/User.Entity";
+import { AppDataSource } from "../.."
+import User from "../../Entities/User.Entity"
 
 export default async function getUserById(id: string): Promise<User[]>{
     if(id !== undefined){
         try{
             return await AppDataSource.manager.find(User,{where:{
                 id
-            }});
+            }})
         } catch(e) {
             console.error(e)
         }
