3fdb543abec3b0a327340e4bc8bcfa390c242123
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-19 14:29:32 +0300

    added delete to the router

--- lib/routes/cv/delete.ts
+++ lib/routes/cv/delete.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from "express"
+import deleteCv from "../../db/queries/cv/delete"
 
-export default function deleteCv(req: Request, res: Response) {
+export default async function deleteUserCv(req: Request, res: Response) {
+    try{
+        await deleteCv(req.params.id)
+        return res.sendStatus(200)
+    } catch(e) {
+        console.error(e)
+        return res.sendStatus(500)
+    }
     res.sendStatus(200)
 }
