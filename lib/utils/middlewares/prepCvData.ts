15e3a69f8ae00e723ed83fa9c0d5baacd125f298
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-19 13:32:47 +0300

    prepping cv data to be inserted into the db

--- lib/utils/middlewares/prepCvData.ts
+++ lib/utils/middlewares/prepCvData.ts
@@ -1,20 +1,71 @@
-import { NextFunction, Request, Response } from "express";
-import Cv from "../../db/Entities/Cv.Entity";
-import UserDetails from "../../db/Entities/UserDetails.Entity";
+import { NextFunction, Request, Response } from "express"
+import { EducationBodyData, EducationData, PersonalSkillsBodyData, WorkExperienceBodyData } from "../../common/types"
+import Cv from "../../db/Entities/Cv.Entity"
+import Education from "../../db/Entities/Education.Entity"
+import Experience from "../../db/Entities/Experience.Entity"
+import PersonalSkills from "../../db/Entities/PersonalSkills.Entity"
+import UserDetails from "../../db/Entities/UserDetails.Entity"
+import WorkExperience from "../../db/Entities/WorkExperience.Entity"
 
 export default function prepCvData(req: Request, res: Response, next: NextFunction) {
-    const cvData: Cv = new Cv();
-    cvData.color = req.body.color;
-    cvData.type = req.body.type;
-    cvData.name = req.body.name;
-    cvData.email = req.body.email;
+    const education: Education[] = [] as Education[]
+    req.body.education.forEach((e: EducationBodyData, i: number) => {
+        const experience: Experience = new Experience()
+        experience.name = e.name
+        experience.town = e.town
+        experience.country = e.country
+        experience.startDate = e.startDate
+        experience.stillEngaged = e.stillEngaged
+        experience.endDate = e.endDate
+        experience.information = e.information
+
+        education[i] = new Education()
+        education[i].type = e.type
+        education[i].experience = experience
+    })
     
-    cvData.details = new UserDetails();
-    cvData.details.address = req.body.address;
-    cvData.details.town = req.body.town;
-    cvData.details.country = req.body.country;
-    cvData.details.phoneNumber = req.body.phoneNumber;
+    const workExperience: WorkExperience[] = [] as WorkExperience[]
+    req.body.workExperience.forEach((e: WorkExperienceBodyData, i: number) => {
+        const experience: Experience = new Experience()
+        experience.name = e.name
+        experience.town = e.town
+        experience.country = e.country
+        experience.startDate = e.startDate
+        experience.stillEngaged = e.stillEngaged
+        experience.endDate = e.endDate
+        experience.information = e.information
+
+        workExperience[i] = new WorkExperience()
+        workExperience[i].type = e.type
+        workExperience[i].jobTitle = e.jobTitle
+        workExperience[i].experience = experience
+    })
+
+    const personalSkills: PersonalSkills[] = [] as PersonalSkills[]
+    req.body.personalSkills.forEach((e: PersonalSkillsBodyData, i: number) => {
+        personalSkills[i] = new PersonalSkills()
+        personalSkills[i].name = e.name
+        personalSkills[i].level = e.level
+    })
     
-    res.locals.cvData = cvData;
-    next();
+    
+    const details = new UserDetails()
+    details.address = req.body.address
+    details.town = req.body.town
+    details.country = req.body.country
+    details.phoneNumber = req.body.phoneNumber
+
+    const cvData: Cv = new Cv()
+    cvData.color = req.body.color
+    cvData.type = req.body.type
+    cvData.name = req.body.name
+    cvData.email = req.body.email
+    cvData.user = req.body.userId
+    cvData.details = details
+    cvData.education = education
+    cvData.workExperience = workExperience
+    cvData.personalSkills = personalSkills
+    
+    res.locals.cvData = cvData
+    next()
 }
