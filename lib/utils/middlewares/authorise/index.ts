8dab71e65120205c4d89eb4e1fd99823f6103078
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-19 13:35:02 +0300

    moved and reworked the authorise function

---
+++ lib/utils/middlewares/authorise/index.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+
+// type return_type = ReturnType<(req: Request, res: Response, next: NextFunction) => Promise<void>>
+
+export default function authorise(getTarget: Function){
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const target: string = await getTarget(req, res)
+
+        if(!target) {
+            res.sendStatus(404)
+        }
+
+        if(target !== res.locals.user.id){
+            if(res.locals.user.admin){
+                next()
+            }
+            res.sendStatus(403)
+        }
+        next()
+    }
+}
