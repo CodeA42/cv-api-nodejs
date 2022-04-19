5f074c047240972cefa9bbac52d4673419122a0e
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-19 13:33:24 +0300

    new routing system

--- lib/routes/cv/index.ts
+++ lib/routes/cv/index.ts
@@ -1,18 +1,25 @@
-import { Router, Request, Response } from "express";
-import authenticate from "../../utils/middlewares/authenticateUser";
-import getAccessToken from "../../utils/middlewares/authenticateUser/getAccessToken";
-import multer from "../../utils/middlewares/multer";
-import authorise from "../../utils/middlewares/authenticateUser";
-import prepCvData from "../../utils/middlewares/prepCvData";
-import updateCv from "./update";
-import createCv from "./create";
+import { Router, Request, Response } from "express"
+import authenticate from "../../utils/middlewares/authenticateUser"
+import getAccessToken from "../../utils/middlewares/authenticateUser/getAccessToken"
+import multer from "../../utils/middlewares/multer"
+import prepCvData from "../../utils/middlewares/prepCvData"
+import updateCv from "./update"
+import createCv from "./create"
+import changeImage from "./image"
+import authorise from "../../utils/middlewares/authorise"
+import getCv from "./get"
+import fromTargetCv from "../../utils/middlewares/authorise/fromtargetCv"
+import body from "../../utils/middlewares/authorise/body"
 
-const cvRouter: Router = Router();
-cvRouter.post('/', authenticate(getAccessToken), authorise, prepCvData, createCv);
-cvRouter.put('/', authenticate(getAccessToken), authorise, updateCv);
+const cvRouter: Router = Router()
+
+cvRouter.get('/:id', authenticate(getAccessToken), authorise(fromTargetCv(true)), getCv)
+
+cvRouter.post('/', authenticate(getAccessToken), authorise(body), prepCvData, createCv)
+cvRouter.put('/image', authenticate(getAccessToken), multer.single('avatar'), authorise(body), changeImage)
 
 cvRouter.use("*", (req: Request, res: Response) => {
-    res.status(404).json(`Invalid request {${req.originalUrl}}`);
+    res.status(404).json(`Invalid request {${req.originalUrl}}`)
 })
 
-export default cvRouter;
+export default cvRouter
