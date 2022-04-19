524f7ace53c1180462df1a5748aa4da64e2878c2
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-19 14:30:12 +0300

    the default value of attach cv is false

--- lib/utils/middlewares/authorise/fromTargetCv.ts
+++ lib/utils/middlewares/authorise/fromTargetCv.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import Cv from "../../../db/Entities/Cv.Entity";
 import getCvById from "../../../db/queries/cv/get";
 
-export default function fromTargetCv(attachCv: boolean) {
+export default function fromTargetCv(attachCv: boolean = false) {
     return async (req: Request, res: Response): Promise<string | undefined> => {
         if(req.params.id){
             const cvList: Cv[] = await getCvById(req.params.id)
