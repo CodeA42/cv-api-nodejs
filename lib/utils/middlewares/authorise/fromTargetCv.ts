4c358e6e6b5d1ee72fd64f3bf146286b2e40ba94
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-19 17:44:38 +0300

    getcv returns a singe cv now  so no need to accept array

--- lib/utils/middlewares/authorise/fromTargetCv.ts
+++ lib/utils/middlewares/authorise/fromTargetCv.ts
@@ -5,8 +5,7 @@ import getCvById from "../../../db/queries/cv/get";
 export default function fromTargetCv(attachCv: boolean = false) {
     return async (req: Request, res: Response): Promise<string | undefined> => {
         if(req.params.id){
-            const cvList: Cv[] = await getCvById(req.params.id)
-            const cv: Cv = cvList[0]
+            const cv: Cv = await getCvById(req.params.id)
             if(cv){
                 if(attachCv){
                     res.locals.cv = cv;
