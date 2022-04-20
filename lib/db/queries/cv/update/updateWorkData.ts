1aa1b9f63a4968ea3ce6363ba488e2a92b8dc029
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-20 15:03:12 +0300

    updates work data

---
+++ lib/db/queries/cv/update/updateWorkData.ts
@@ -0,0 +1,37 @@
+import { QueryRunner } from "typeorm";
+import { AppDataSource } from "../../..";
+import Cv from "../../../Entities/Cv.Entity";
+import Experience from "../../../Entities/Experience.Entity";
+import WorkExperience from "../../../Entities/WorkExperience.Entity";
+
+export default async function updateWorkData(cv: Cv, cvData: Cv, queryRunner: QueryRunner) {
+    for(const e of cvData.workExperience){
+        let workExperience = await AppDataSource.manager.findOneBy(WorkExperience, {
+            id: e.id
+        })
+
+        if(!workExperience) {
+            workExperience = new WorkExperience()
+        }
+
+        if(e.type) workExperience.type = e.type
+        if(e.jobTitle) workExperience.jobTitle = e.jobTitle
+
+        if(!workExperience.experience) workExperience.experience = new Experience()
+
+        if(e.experience?.name)          workExperience.experience.name = e.experience.name
+        if(e.experience?.town)          workExperience.experience.town = e.experience.town
+        if(e.experience?.country)       workExperience.experience.country = e.experience.country
+        if(e.experience?.startDate)     workExperience.experience.startDate = e.experience.startDate
+        if(e.experience?.stillEngaged)  workExperience.experience.stillEngaged = e.experience.stillEngaged
+        if(e.experience?.endDate)       workExperience.experience.endDate = e.experience.endDate
+        if(e.experience?.information)   workExperience.experience.information = e.experience.information
+
+
+        if(workExperience.id) {
+            await queryRunner.manager.save(workExperience)
+        } else {
+            cv.workExperience.push(workExperience)
+        }
+    }
+}
