484cf6b1702919878e5c8a4c1a7dbac024a2c539
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-20 12:43:51 +0300

    ocd much

--- lib/db/queries/user/update.ts
+++ lib/db/queries/user/update.ts
@@ -6,8 +6,8 @@ export default async function updateUserData(userData: UserData){
     const userRepo = AppDataSource.getRepository(User);
     const user = await userRepo.findOneBy({id: userData.id})
 
-    if(userData.firstName)      user.firstName = userData.firstName
-    if(userData.lastName)       user.lastName = userData.lastName
+    if(userData.firstName)              user.firstName = userData.firstName
+    if(userData.lastName)               user.lastName = userData.lastName
 
     if(userData.details.avatar)         user.details.avatar = userData.details.avatar
     if(userData.details.address)        user.details.address = userData.details.address
@@ -15,5 +15,5 @@ export default async function updateUserData(userData: UserData){
     if(userData.details.country)        user.details.country = userData.details.country
     if(userData.details.phoneNumber)    user.details.phoneNumber = userData.details.phoneNumber
 
-    return await userRepo.save(user);
+    return await userRepo.save(user)
 }
