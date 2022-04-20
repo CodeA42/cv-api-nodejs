4f93f5dd73489c93cdf171ed1882bded4dd248b5
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-20 17:17:17 +0300

    self explanatory name

---
+++ lib/db/queries/cv/deleteWorkExperienceEntity.ts
@@ -0,0 +1,7 @@
+import { AppDataSource } from "../.."
+import WorkExperience from "../../Entities/WorkExperience.Entity"
+
+export default async function deleteWorkExperienceEntity(id: string) {
+    const workExperienceRepo = AppDataSource.getRepository(WorkExperience)
+    return await workExperienceRepo.delete({ id })
+}
