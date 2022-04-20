b89c46e7695ae032ab5c7c04a6f4a0f4b6c48fd4
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-20 14:30:37 +0300

    bit better update version

--- lib/routes/cv/update.ts
+++ lib/routes/cv/update.ts
@@ -1,12 +1,15 @@
-import { Request, Response } from "express";
-import updateCvData from "../../db/queries/cv/update";
+import { Request, Response } from "express"
+import Cv from "../../db/Entities/Cv.Entity"
+import getAllCvData from "../../db/queries/cv/get/getAllCvData"
+import updateCvData from "../../db/queries/cv/update"
 
 export default async function updateCv(req: Request, res: Response) {
     try{
-        await updateCvData(res.locals.cv, res.locals.cvData);
-        return res.sendStatus(200);
+        const cv: Cv = await getAllCvData(req.body.id)
+        const response = await updateCvData(cv, res.locals.cvData)
+        return res.status(200).json(response)
     } catch(e){
-        console.log(e);
-        return res.sendStatus(500);
+        console.log(e)
+        return res.sendStatus(500)
     }
 }
