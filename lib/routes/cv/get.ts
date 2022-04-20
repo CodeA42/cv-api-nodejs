6f234450ade470c7339afc74afebd41c4d4bcf20
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-20 13:00:05 +0300

    simplified get

--- lib/routes/cv/get.ts
+++ lib/routes/cv/get.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
 import Cv from "../../db/Entities/Cv.Entity";
+import getAllCvData from "../../db/queries/cv/get/getAllCvData";
 
 export default async function getCv(req: Request, res: Response) {
-    const cv: Cv = res.locals.cv
+    const cv: Cv = await getAllCvData(req.params.id)
     if(cv) {
-        cv.user = undefined
         res.status(200).json(cv)
     }
 
