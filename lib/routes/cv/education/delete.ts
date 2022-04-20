a72d29a8cd7d58c17043330d3c24de041f42f102
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-20 17:06:07 +0300

    delete education endpoint

---
+++ lib/routes/cv/education/delete.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express"
+import deleteEducationEntity from "../../../db/queries/cv/deleteEducationEntity"
+
+export default async function deleteEducation(req: Request, res: Response) {
+    try{
+        await deleteEducationEntity(req.params.id)
+        return res.sendStatus(200)
+    } catch(e) {
+        console.error(e)
+        return res.sendStatus(500)
+    }
+}
