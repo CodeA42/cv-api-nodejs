d7f9bee3a752a79bdadff42d2fb9b7b5875eb3d2
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-20 12:52:40 +0300

    moved cv to its own folder

--- lib/db/queries/cv/get/get.ts
+++ lib/db/queries/cv/get/get.ts
@@ -1,5 +1,6 @@
-import { AppDataSource } from "../..";
-import Cv from "../../Entities/Cv.Entity";
+import { AppDataSource } from "../../..";
+import Cv from "../../../Entities/Cv.Entity";
+import User from "../../../Entities/User.Entity";
 
 export default async function getCvById(id: string): Promise<Cv> {
     if(id !== undefined){
