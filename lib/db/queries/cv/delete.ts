d7f65dea4b5d422d68cb6d1a14d3a7b5bd6ed2a8
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-20 14:29:58 +0300

    it should delete well now

--- lib/db/queries/cv/delete.ts
+++ lib/db/queries/cv/delete.ts
@@ -1,9 +1,9 @@
-import { AppDataSource } from "../..";
-import Cv from "../../Entities/Cv.Entity";
-import Education from "../../Entities/Education.Entity";
-import Experience from "../../Entities/Experience.Entity";
-import UserDetails from "../../Entities/UserDetails.Entity";
-import WorkExperience from "../../Entities/WorkExperience.Entity";
+import { AppDataSource } from "../.."
+import Cv from "../../Entities/Cv.Entity"
+import Education from "../../Entities/Education.Entity"
+import Experience from "../../Entities/Experience.Entity"
+import UserDetails from "../../Entities/UserDetails.Entity"
+import WorkExperience from "../../Entities/WorkExperience.Entity"
 
 export default async function deleteCv(id: string) {
     const cvRepo = AppDataSource.getRepository(Cv)
@@ -11,24 +11,27 @@ export default async function deleteCv(id: string) {
 
     const queryRunner = AppDataSource.createQueryRunner()
     await queryRunner.connect()
-    await queryRunner.startTransaction()
-
+    await queryRunner.startTransaction("SERIALIZABLE")
+    
     try {
-        if(cv.details?.id) await queryRunner.manager.delete(UserDetails,{where: {id: cv.details.id}})
+        if(cv.details?.id) await queryRunner.manager.delete(UserDetails, { id: cv.details.id })
     
         const education: Education[] = cv.education
         education.forEach(async (e: Education) => {
-            await queryRunner.manager.delete(Experience, {where: {id: e.experience.id}})
+            await queryRunner.manager.delete(Experience, { id: e.experience.id })
         });
     
         const workExperience: WorkExperience[] = cv.workExperience
         workExperience.forEach(async (e: WorkExperience) => {
-            await queryRunner.manager.delete(Experience, {where: {id: e.experience.id}})
+            await queryRunner.manager.delete(Experience, { id: e.experience.id })
         })
-    
-        await queryRunner.manager.delete(Cv, {where: {id}})
+        
+        await queryRunner.manager.createQueryBuilder().delete().from(Cv).where('id = :id', {id: cv.id}).execute()
+        
+        await queryRunner.commitTransaction()
     } catch (e) {
         await queryRunner.rollbackTransaction()
+        throw e
     } finally {
         await queryRunner.release()
     }    
