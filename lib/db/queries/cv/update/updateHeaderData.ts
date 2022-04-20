19da4c0422a8d7f42838e2e1020eb9c277d58f98
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-20 15:02:41 +0300

    updated given cv header data  with some given data and a given query runner

---
+++ lib/db/queries/cv/update/updateHeaderData.ts
@@ -0,0 +1,22 @@
+import { QueryRunner } from "typeorm"
+import Cv from "../../../Entities/Cv.Entity"
+import UserDetails from "../../../Entities/UserDetails.Entity"
+
+export default async function updateHeaderData(cv: Cv, cvData: Cv, queryRunner: QueryRunner) {
+    if(cvData.color) cv.color = cvData.color
+    if(cvData.type) cv.type = cvData.type
+    if(cvData.name) cv.name = cvData.name
+    if(cvData.email) cv.email = cvData.email
+    
+    if(!cv.details){
+        cv.details = new UserDetails()
+    }
+
+    if(cvData.details.avatar) cv.details.avatar = cvData.details.avatar
+    if(cvData.details.address) cv.details.address = cvData.details.address
+    if(cvData.details.town) cv.details.town = cvData.details.town
+    if(cvData.details.country) cv.details.country = cvData.details.country
+    if(cvData.details.phoneNumber) cv.details.phoneNumber = cvData.details.phoneNumber
+
+    return await queryRunner.manager.save(cv)
+}
