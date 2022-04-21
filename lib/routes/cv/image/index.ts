03286198cae08fe0278a764be6cdc145f8d0e503
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 15:29:29 +0300

    added put image endpoint

--- lib/routes/cv/image/index.ts
+++ lib/routes/cv/image/index.ts
@@ -2,12 +2,17 @@ import { Request, Response, Router } from "express"
 import authenticate from "../../../utils/middlewares/authenticateUser"
 import getAccessToken from "../../../utils/middlewares/authenticateUser/getAccessToken"
 import authorise from "../../../utils/middlewares/authorise"
+import bodyTargetCv from "../../../utils/middlewares/authorise/bodyTargetCv"
 import fromTargetPersonalSkillEntity from "../../../utils/middlewares/authorise/fromTargetPersonalSkillEntity"
+import multer from "../../../utils/middlewares/multer"
 import deleteImage from "./delete"
+import putImage from "./put"
 
 const imageRouter: Router = Router()
 
 imageRouter.delete('/:id', authenticate(getAccessToken), authorise(fromTargetPersonalSkillEntity), deleteImage)
+
+imageRouter.put('/', authenticate(getAccessToken), multer.single('avatar'), authorise(bodyTargetCv), putImage)
 
 imageRouter.use("*", (req: Request, res: Response) => {
     res.status(404).json(`Invalid request {${req.originalUrl}}`)
