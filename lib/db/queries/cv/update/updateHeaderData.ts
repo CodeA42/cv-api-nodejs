80bb874ac47bbd9ebc12c838c487b77082598d83
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 15:25:38 +0300

    avatar should not be set here

--- lib/db/queries/cv/update/updateHeaderData.ts
+++ lib/db/queries/cv/update/updateHeaderData.ts
@@ -12,7 +12,6 @@ export default async function updateHeaderData(cv: Cv, cvData: Cv, queryRunner: 
         cv.details = new UserDetails()
     }
     if(cvData.details) {
-        if(cvData.details.avatar) cv.details.avatar = cvData.details.avatar
         if(cvData.details.address) cv.details.address = cvData.details.address
         if(cvData.details.town) cv.details.town = cvData.details.town
         if(cvData.details.country) cv.details.country = cvData.details.country
