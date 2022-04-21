60be01bc23d650bd774b96b12aef2a9cf9cba19f
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 16:11:06 +0300

    added delete and get image paths

--- lib/routes/cv/image/index.ts
+++ lib/routes/cv/image/index.ts
@@ -4,15 +4,19 @@ import getAccessToken from "../../../utils/middlewares/authenticateUser/getAcces
 import authorise from "../../../utils/middlewares/authorise"
 import bodyTargetCv from "../../../utils/middlewares/authorise/bodyTargetCv"
 import fromTargetPersonalSkillEntity from "../../../utils/middlewares/authorise/fromTargetPersonalSkillEntity"
+import paramTargetImage from "../../../utils/middlewares/authorise/paramTargetImage"
 import multer from "../../../utils/middlewares/multer"
 import deleteImage from "./delete"
+import getImage from "./get"
 import putImage from "./put"
 
 const imageRouter: Router = Router()
 
-imageRouter.delete('/:id', authenticate(getAccessToken), authorise(fromTargetPersonalSkillEntity), deleteImage)
+imageRouter.get("/:id", authenticate(getAccessToken), authorise(paramTargetImage), getImage), 
 
 imageRouter.put('/', authenticate(getAccessToken), multer.single('avatar'), authorise(bodyTargetCv), putImage)
+
+imageRouter.delete('/:id', authenticate(getAccessToken), authorise(fromTargetPersonalSkillEntity), deleteImage)
 
 imageRouter.use("*", (req: Request, res: Response) => {
     res.status(404).json(`Invalid request {${req.originalUrl}}`)
