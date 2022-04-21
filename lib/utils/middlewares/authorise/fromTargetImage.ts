65b31f2f4e7738798b12fb3ffebeb5e3cee60a7a
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 17:31:18 +0300

    some more try catching

--- lib/utils/middlewares/authorise/fromTargetImage.ts
+++ lib/utils/middlewares/authorise/fromTargetImage.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
 import User from "../../../db/Entities/User.Entity";
 import findUserIdFromImageId from "../../../db/queries/cv/image/findUserIdFromImageId";
+import ImageOrUserNotFoundError from "../../../error/ImageOrUserNotFoundError";
+import MissingImageIdError from "../../../error/MissingImageIdError";
 
 export default async function fromTargetImage(req: Request, res: Response) {
     if(req.params.id){
-        const user: User = await findUserIdFromImageId(req.params.id)
-        if(user) return user.id
+        try{
+            const user: User = await findUserIdFromImageId(req.params.id)
+            if(user) return user.id
+        } catch(e) {
+            if(e instanceof ImageOrUserNotFoundError) {
+                return res.status(404).json({name: e.name})
+            }
+        }
         return undefined
     }
-    return undefined
+    throw new MissingImageIdError("Missing image id")
 }
