b8f8d7d296ac813961041ca61bf4e4650cf20474
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 17:31:31 +0300

    even more try catching

--- lib/utils/middlewares/authorise/fromTargetPersonalSkillEntity.ts
+++ lib/utils/middlewares/authorise/fromTargetPersonalSkillEntity.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express"
 import User from "../../../db/Entities/User.Entity"
-import findUserIdFromPersonalSkillId from "../../../db/queries/cv/get/findUserIdFromPersonalSkillId"
+import findUserIdFromImageId from "../../../db/queries/cv/image/findUserIdFromImageId"
+import ImageOrUserNotFoundError from "../../../error/ImageOrUserNotFoundError"
+import MissingPersonalSkillIdError from "../../../error/MissingPersonalSkillIdError"
 
 export default async function fromTargetPersonalSkillEntity(req: Request, res: Response) {
     if(req.params.id){
-        const user: User = await findUserIdFromPersonalSkillId(req.params.id)
-        if(user) return user.id
+        try {
+            const user: User = await findUserIdFromImageId(req.params.id)
+            if(user) return user.id
+        } catch(e) {
+            if(e instanceof ImageOrUserNotFoundError){
+                res.status(404).json({name: e.name})
+            }
+        }
         return undefined
     }
-    return undefined
+    throw new MissingPersonalSkillIdError("Missing personal skill id")
 }
