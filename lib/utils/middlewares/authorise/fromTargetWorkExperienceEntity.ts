215a2d383ccacaac4ef15a3e81a19da836b4a46a
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 17:33:14 +0300

    moar ahahahahah

--- lib/utils/middlewares/authorise/fromTargetWorkExperienceEntity.ts
+++ lib/utils/middlewares/authorise/fromTargetWorkExperienceEntity.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express"
 import User from "../../../db/Entities/User.Entity"
 import findUserIdFromWorkExperienceId from "../../../db/queries/cv/get/findUserIdFromWorkExperienceId"
+import MissingWorkExperienceEntity from "../../../error/MissingWorkExperienceEntity"
+import WorkExperienceOrUserNotFound from "../../../error/WorkExperienceOrUserNotFound"
 
 export default async function fromTargetWorkExperienceEntity(req: Request, res: Response) {
     if(req.params.id){
-        const user: User = await findUserIdFromWorkExperienceId(req.params.id)
-        if(user) return user.id
+        try {
+            const user: User = await findUserIdFromWorkExperienceId(req.params.id)
+            if(user) return user.id
+        } catch(e) {
+            if(e instanceof WorkExperienceOrUserNotFound){
+                return res.status(404).json({name: e.name})
+            }
+        }
         return undefined
     }
-    return undefined
+    throw new MissingWorkExperienceEntity("Missing work experience entity")
 }
