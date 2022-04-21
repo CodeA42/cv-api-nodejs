60f98ed54914360c3348b892c010038c3156c085
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 17:13:17 +0300

    checks and balances here too he he

--- lib/db/queries/user/getUserIdByCvId.ts
+++ lib/db/queries/user/getUserIdByCvId.ts
@@ -1,13 +1,24 @@
 import { AppDataSource } from "../..";
+import CvOrUserNotFoundError from "../../../error/CvOrUserNotFoundError";
+import MissingCvIdError from "../../../error/MissingCvIdError";
 import User from "../../Entities/User.Entity";
 
 export default async function getUserIdFromCvId(id: string): Promise<User> {
-    return await AppDataSource
-    .manager
-    .createQueryBuilder()
-    .select("user.id")
-    .from(User,"user")
-    .leftJoin("user.cvs", "cv")
-    .where("cv.id = :id", {id})
-    .getOne()
+    if(id) {
+        try {
+            const user: User = await AppDataSource
+            .manager
+            .createQueryBuilder()
+            .select("user.id")
+            .from(User,"user")
+            .leftJoin("user.cvs", "cv")
+            .where("cv.id = :id", {id})
+            .getOne()
+            if(user) return user
+            throw new CvOrUserNotFoundError("Cv or user not found")
+        } catch(e) {
+            console.error(e);
+        }
+    }
+    throw new MissingCvIdError("Missing cv id")
 }
