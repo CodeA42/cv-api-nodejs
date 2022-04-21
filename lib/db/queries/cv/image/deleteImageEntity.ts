671a84013c6d72c3d1d98a00d0a4a28e59fb80cb
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 17:11:19 +0300

    there was a missing import

--- lib/db/queries/cv/image/deleteImageEntity.ts
+++ lib/db/queries/cv/image/deleteImageEntity.ts
@@ -1,4 +1,5 @@
 import { AppDataSource } from "../../..";
+import Image from "../../../Entities/Image.Entity";
 
 export default async function deleteImageEntity(id: string) {
     return await AppDataSource.manager.delete(Image, { id })
