783fe0568f36f24c9915a1ed2e18ec54635f3baf
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 16:10:09 +0300

    added check if the image is missing

--- lib/routes/cv/image/delete.ts
+++ lib/routes/cv/image/delete.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express"
 import deleteImageEntity from "../../../db/queries/cv/image/deleteImageEntity"
+import ImageNotFoundError from "../../../error/ImageNotFoundError"
 
 export default async function deleteImage(req: Request, res: Response) {
     try{
         await deleteImageEntity(req.params.id)
         return res.sendStatus(200)
     } catch(e) {
+        if(e instanceof ImageNotFoundError) {
+            return res.sendStatus(404)
+        }
         console.error(e)
         return res.sendStatus(500)
     }
