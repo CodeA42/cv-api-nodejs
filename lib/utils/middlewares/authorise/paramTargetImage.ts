01ad09c7ad820c5c3dde5275e5d4eb6747fddb70
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 17:33:05 +0300

    even mooore try catching

--- lib/utils/middlewares/authorise/paramTargetImage.ts
+++ lib/utils/middlewares/authorise/paramTargetImage.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
 import User from "../../../db/Entities/User.Entity";
 import findUserIdFromImageId from "../../../db/queries/cv/image/findUserIdFromImageId";
+import ImageNotFoundError from "../../../error/ImageNotFoundError";
+import ImageOrUserNotFoundError from "../../../error/ImageOrUserNotFoundError";
 
 export default async function paramTargetImage(req: Request, res: Response) {
     if(req.params.id){
-        const user: User = await findUserIdFromImageId(req.params.id)
-        if(user) return user.id
+        try {
+            const user: User = await findUserIdFromImageId(req.params.id)
+            if(user) return user.id
+        } catch (e) {
+            if(e instanceof ImageOrUserNotFoundError) {
+                return res.sendStatus(404).json(e.message)
+            }
+        }
         return undefined
     }
-    return undefined
+    throw new ImageNotFoundError("Image not found")
 }
