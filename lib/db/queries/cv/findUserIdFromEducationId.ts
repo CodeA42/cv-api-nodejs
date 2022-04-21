759865d006f3097f2c5068001e50da8cfacb41db
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 09:54:12 +0300

    bit of house cleaning

--- lib/db/queries/cv/findUserIdFromEducationId.ts
+++ lib/db/queries/cv/findUserIdFromEducationId.ts
@@ -4,14 +4,16 @@ import User from "../../Entities/User.Entity";
 export default async function findUserIdFromEducationId(id: string): Promise<User> {
     if(id !== undefined){
         try{
-            return await AppDataSource.manager.createQueryBuilder()
-                                        .select("user.id")
-                                        .from(User, "user")
-                                        .leftJoin("user.cvs", "cv")
-                                        .leftJoin("cv.education","education")
-                                        .where("education.id = :id", {id})
-                                        .getOne()
-            
+            return await AppDataSource
+            .manager
+            .createQueryBuilder()
+            .select("user.id")
+            .from(User, "user")
+            .leftJoin("user.cvs", "cv")
+            .leftJoin("cv.education","education")
+            .where("education.id = :id", {id})
+            .getOne()
+
         } catch(e) {
             console.error(e)
             return undefined
