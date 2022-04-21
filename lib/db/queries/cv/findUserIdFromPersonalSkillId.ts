7c4b8a3efc7d67d34dcc517f93e82b9549fb8ca6
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 09:54:43 +0300

    self explanatory name

---
+++ lib/db/queries/cv/findUserIdFromPersonalSkillId.ts
@@ -0,0 +1,22 @@
+import { AppDataSource } from "../..";
+import User from "../../Entities/User.Entity";
+
+export default async function findUserIdFromEducationId(id: string): Promise<User> {
+    if(id !== undefined){
+        try{
+            return await AppDataSource
+            .manager
+            .createQueryBuilder()
+            .select("user.id")
+            .from(User, "user")
+            .leftJoin("user.cvs", "cv")
+            .leftJoin("cv.personalSkills","personalSkills")
+            .where("personalSkills.id = :id", {id})
+            .getOne()
+        } catch(e) {
+            console.error(e)
+            return undefined
+        }
+    }
+    return undefined
+}
