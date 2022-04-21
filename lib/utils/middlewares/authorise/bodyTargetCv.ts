4fcb0c285a566934436304d74d216abb6cd1dcbc
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 17:11:45 +0300

    throwing some errors in case stuff is missing

--- lib/utils/middlewares/authorise/bodyTargetCv.ts
+++ lib/utils/middlewares/authorise/bodyTargetCv.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
 import User from "../../../db/Entities/User.Entity";
 import getUserIdFromCvId from "../../../db/queries/user/getUserIdByCvId";
+import CvOrUserNotFoundError from "../../../error/CvOrUserNotFoundError";
+import MissingCvIdError from "../../../error/MissingCvIdError";
 
 export default async function bodyTargetCv(req: Request, res: Response) {
     if(req.body.cvId){
-        const user: User = await getUserIdFromCvId(req.body.cvId)
-        if(user) return user.id
+        try {
+            const user: User = await getUserIdFromCvId(req.body.cvId)
+            if(user) return user.id
+        } catch(e) {
+            if(e instanceof CvOrUserNotFoundError) {
+                return res.sendStatus(404).json({name: e.name})
+            }
+        }
         return undefined
     }
-    return undefined
+    throw new MissingCvIdError("Missing cv id")
 }
