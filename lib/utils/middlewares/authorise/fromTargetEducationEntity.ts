b860354192ddcbae526d8d3a9cafeaa62f21f3d7
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 17:11:57 +0300

    throwing some errors

--- lib/utils/middlewares/authorise/fromTargetEducationEntity.ts
+++ lib/utils/middlewares/authorise/fromTargetEducationEntity.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
 import User from "../../../db/Entities/User.Entity";
 import findUserIdFromEducationId from "../../../db/queries/cv/get/findUserIdFromEducationId";
+import EducationOrUserNotFound from "../../../error/EducationOrUserNotFound";
+import MissingEducationId from "../../../error/MissingEducationId";
 
 export default async function fromTargetEducationEntity(req: Request, res: Response) {
     if(req.params.id){
-        const user: User = await findUserIdFromEducationId(req.params.id)
-        if(user) return user.id
+        try{
+            const user: User = await findUserIdFromEducationId(req.params.id)
+            if(user) return user.id
+        } catch(e) {
+            if(e instanceof EducationOrUserNotFound) {
+                return res.status(404).json({name: e.name})
+            }            
+        }
         return undefined
     }
-    return undefined
+    throw new MissingEducationId("Missing education id");
 }
