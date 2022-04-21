11e2d220fb93f22d013a4741eadaf6ac033766f1
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 09:53:51 +0300

    self expanatory name

---
+++ lib/db/queries/cv/deletePersonalSkillEntity.ts
@@ -0,0 +1,7 @@
+import { AppDataSource } from "../.."
+import PersonalSkills from "../../Entities/PersonalSkills.Entity"
+
+export default async function deletePersonalSkillEntity(id: string) {
+    const personalSkillsRepo = AppDataSource.getRepository(PersonalSkills)
+    return await personalSkillsRepo.delete({ id })
+}
