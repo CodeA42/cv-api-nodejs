dccc38fe5b64a9687742f46988d6e82ebd8a0332
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 15:26:25 +0300

    looks better?

--- lib/db/queries/user/getUserIdByCvId.ts
+++ lib/db/queries/user/getUserIdByCvId.ts
@@ -2,10 +2,12 @@ import { AppDataSource } from "../..";
 import User from "../../Entities/User.Entity";
 
 export default async function getUserIdFromCvId(id: string): Promise<User> {
-    return await AppDataSource.manager.createQueryBuilder()
-                                        .select("user.id")
-                                        .from(User,"user")
-                                        .leftJoin("user.cvs", "cv")
-                                        .where("cv.id = :id", {id})
-                                        .getOne()
+    return await AppDataSource
+    .manager
+    .createQueryBuilder()
+    .select("user.id")
+    .from(User,"user")
+    .leftJoin("user.cvs", "cv")
+    .where("cv.id = :id", {id})
+    .getOne()
 }
