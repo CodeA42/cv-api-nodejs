2ab6a5cac9b7e47ecbb65314f84fba7397bb8c10
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 09:57:55 +0300

    was calling wrong function

--- lib/utils/middlewares/authorise/fromTargetPersonalSkillEntity.ts
+++ lib/utils/middlewares/authorise/fromTargetPersonalSkillEntity.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express"
 import User from "../../../db/Entities/User.Entity"
-import findUserIdFromWorkExperienceId from "../../../db/queries/cv/findUserIdFromWorkExperienceId"
+import findUserIdFromPersonalSkillId from "../../../db/queries/cv/findUserIdFromPersonalSkillId"
 
 export default async function fromTargetPersonalSkillEntity(req: Request, res: Response) {
     if(req.params.id){
-        const user: User = await findUserIdFromWorkExperienceId(req.params.id)
+        const user: User = await findUserIdFromPersonalSkillId(req.params.id)
         if(user) return user.id
         return undefined
     }
