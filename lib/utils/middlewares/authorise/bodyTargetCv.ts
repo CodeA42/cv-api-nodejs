88b580b7d099695efa3b62d606ee073f6081edb1
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 15:30:24 +0300

    there was a missing return

--- lib/utils/middlewares/authorise/bodyTargetCv.ts
+++ lib/utils/middlewares/authorise/bodyTargetCv.ts
@@ -3,9 +3,10 @@ import User from "../../../db/Entities/User.Entity";
 import getUserIdFromCvId from "../../../db/queries/user/getUserIdByCvId";
 
 export default async function bodyTargetCv(req: Request, res: Response) {
-    if(req.body.id){
-        const user: User = await getUserIdFromCvId(req.body.id)
+    if(req.body.cvId){
+        const user: User = await getUserIdFromCvId(req.body.cvId)
         if(user) return user.id
         return undefined
     }
+    return undefined
 }
