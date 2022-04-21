7107009c12667813ec4c557fa26757dd0421c419
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-21 15:21:23 +0300

    added image entity to connection

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 import Cv from "./Entities/Cv.Entity";
 import Edication from "./Entities/Education.Entity";
 import Experience from "./Entities/Experience.Entity";
+import Image from "./Entities/Image.Entity";
 import PersonalSkills from "./Entities/PersonalSkills.Entity";
 import User from "./Entities/User.Entity";
 import UserDetails from "./Entities/UserDetails.Entity";
@@ -14,7 +15,7 @@ export const AppDataSource = new DataSource({
     username: "cvapiuser",
     password: "supersafepassword",
     database: "cvapi",
-    entities:[User, Cv, Edication, Experience, PersonalSkills, WorkExperience, UserDetails],
+    entities:[User, Cv, Edication, Experience, PersonalSkills, WorkExperience, UserDetails, Image],
     synchronize: true
 });
 
