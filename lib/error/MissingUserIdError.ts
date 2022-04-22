574ab9623ae5f5e62face03b2800c4ca9fd62ea1
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:27:58 +0300

    some more errors

--- lib/error/MissingUserIdError.ts
+++ lib/error/MissingUserIdError.ts
@@ -1,4 +1,5 @@
 export default class MissingUserIdError extends Error {
+    static defaultMessage: string = "Missing user id"
     constructor(message: string){
         super(message)
         this.name = "MissingUserIdError"
