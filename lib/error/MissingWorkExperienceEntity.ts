99bcd58a951265606ad6d0d419538c738fa5df76
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:25:26 +0300

    no need for that one

--- lib/error/MissingWorkExperienceEntity.ts
+++
@@ -1,6 +0,0 @@
-export default class MissingWorkExperienceEntity extends Error {
-    constructor(message: string){
-        super(message)
-        this.name = "MissingWorkExperienceEntity"
-    }
-}
