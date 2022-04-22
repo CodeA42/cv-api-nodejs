654f729ec4d25df852e14dd6ee7542059da754a6
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-23 00:06:38 +0300

    should be fairly safe now

--- lib/db/queries/user/getUserIdByCvId.ts
+++ lib/db/queries/user/getUserIdByCvId.ts
@@ -3,7 +3,7 @@ import CvOrUserNotFoundError from "../../../error/CvOrUserNotFoundError";
 import MissingCvIdError from "../../../error/MissingCvIdError";
 import User from "../../Entities/User.Entity";
 
-export default async function getUserIdFromCvId(id: string): Promise<User> {
+export default async function getUserIdFromCvId(id: string): Promise<User | null> {
     if(id) {
         try {
             const user: User = await AppDataSource
@@ -15,10 +15,11 @@ export default async function getUserIdFromCvId(id: string): Promise<User> {
             .where("cv.id = :id", {id})
             .getOne()
             if(user) return user
-            throw new CvOrUserNotFoundError("Cv or user not found")
+            throw new CvOrUserNotFoundError(CvOrUserNotFoundError.defaultMessage)
         } catch(e) {
-            console.error(e);
+            console.error(e)
+            return null
         }
     }
-    throw new MissingCvIdError("Missing cv id")
+    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
 }
