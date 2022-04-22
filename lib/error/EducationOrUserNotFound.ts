000da926ead0a1a86264bdc85cd2a13d6dc293f3
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:24:32 +0300

    added default message

--- lib/error/EducationOrUserNotFound.ts
+++ lib/error/EducationOrUserNotFound.ts
@@ -1,4 +1,5 @@
 export default class EducationOrUserNotFound extends Error {
+    static defaultMessage: string = "Education or user not found"
     constructor(message: string){
         super(message)
         this.name = "EducationOrUserNotFound"
