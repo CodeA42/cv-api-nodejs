918a9f32f4a28d84e5c7e19d971e3f1c424bb04c
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-23 00:07:04 +0300

    new error

---
+++ lib/error/UserDetailsNotFoundError.ts
@@ -0,0 +1,7 @@
+export default class UserDetailsNotFoundError extends Error {
+    static defaultMessage: string = "User details not found"
+    constructor(message: string){
+        super(message)
+        this.name = "UserDetailsNotFoundError"
+    }
+}
