53759880f0e915ed56162630a2ec180dd2e06538
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-23 00:05:27 +0300

    added error handling and proper return types

--- lib/db/queries/cv/get/getImageEntityWithCvId.ts
+++ lib/db/queries/cv/get/getImageEntityWithCvId.ts
@@ -1,14 +1,26 @@
 import { AppDataSource } from "../../..";
+import ImageNotFoundError from "../../../../error/ImageNotFoundError";
+import MissingCvIdError from "../../../../error/MissingCvIdError";
 import Image from "../../../Entities/Image.Entity";
 
-export default async function getImageEntityWithCvId(id: String): Promise<Image> {
-    return await AppDataSource
-    .manager
-    .createQueryBuilder()
-    .select("image")
-    .from(Image, "image")
-    .leftJoin("image.userDetails", "userDetails")
-    .leftJoin("userDetails.cv", "cv")
-    .where("cv.id = :id", { id })
-    .getOne()
+export default async function getImageEntityWithCvId(id: String): Promise<Image | null> {
+    if(id){
+        try {
+            const image: Image = await AppDataSource
+            .manager
+            .createQueryBuilder()
+            .select("image")
+            .from(Image, "image")
+            .leftJoin("image.userDetails", "userDetails")
+            .leftJoin("userDetails.cv", "cv")
+            .where("cv.id = :id", { id })
+            .getOne()
+            if(image) return image
+            throw new ImageNotFoundError(ImageNotFoundError.defaultMessage)
+        } catch(e){
+            console.error(e);
+            return null
+        }
+    }
+    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
 }
