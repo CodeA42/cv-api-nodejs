b16f47c9815bf427b8140d25b18b7cc7aa16eb45
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-23 00:04:27 +0300

    even more error handling

--- lib/db/queries/cv/get/getCvWithDetails.ts
+++ lib/db/queries/cv/get/getCvWithDetails.ts
@@ -1,12 +1,24 @@
 import { AppDataSource } from "../../..";
+import CvNotFoundError from "../../../../error/CvNotFoundError";
+import MissingCvIdError from "../../../../error/MissingCvIdError";
 import Cv from "../../../Entities/Cv.Entity";
 
-export default async function getCvWithDetails(id: string) {
-    return await AppDataSource
-    .manager
-    .createQueryBuilder()
-    .select("cv")
-    .from(Cv, "cv")
-    .leftJoinAndSelect("cv.details","details")
-    .getOne()
+export default async function getCvWithDetails(id: string): Promise<Cv | null> {
+    if(id){
+        try{
+            const cv: Cv = await AppDataSource
+            .manager
+            .createQueryBuilder()
+            .select("cv")
+            .from(Cv, "cv")
+            .leftJoinAndSelect("cv.details","details")
+            .getOne()
+            if(cv) return cv
+            throw new CvNotFoundError(CvNotFoundError.defaultMessage) 
+        } catch(e) {
+            console.error(e);
+            return null
+        }
+    }
+    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
 }
