cf02767cc0f79b0797bc795424834d3c547c676d
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:24:37 +0300

    added default message

--- lib/error/ImageNotFoundError.ts
+++ lib/error/ImageNotFoundError.ts
@@ -1,4 +1,5 @@
 export default class ImageNotFoundError extends Error {
+    static defaultMessage: string = "Image not found"
     constructor(message: string){
         super(message)
         this.name = "ImageNotFoundError"
