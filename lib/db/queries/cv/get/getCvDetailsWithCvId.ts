1dd6190eb3e852ec8bd7cd2e76d2fea84f749cb9
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-23 00:05:06 +0300

    error handling and proper return types

--- lib/db/queries/cv/get/getCvDetailsWithCvId.ts
+++ lib/db/queries/cv/get/getCvDetailsWithCvId.ts
@@ -1,14 +1,28 @@
 import { AppDataSource } from "../../..";
+import MissingCvIdError from "../../../../error/MissingCvIdError";
+import UserDetailsNotFoundError from "../../../../error/UserDetailsNotFoundError";
 import UserDetails from "../../../Entities/UserDetails.Entity";
 
-export default async function getCvDetailsWithCvId(id: String): Promise<UserDetails> {
-    return await AppDataSource
-    .manager
-    .createQueryBuilder()
-    .from(UserDetails, "details")
-    .select("details")
-    .leftJoin("details.cv", "cv")
-    .leftJoinAndSelect("details.image", "image")
-    .where("cv.id = :id", { id })
-    .getOne()
+export default async function getCvDetailsWithCvId(id: String): Promise<UserDetails | null> {
+    if(id) {
+        try{
+            const userDetails: UserDetails = await AppDataSource
+            .manager
+            .createQueryBuilder()
+            .from(UserDetails, "details")
+            .select("details")
+            .leftJoin("details.cv", "cv")
+            .leftJoinAndSelect("details.image", "image")
+            .where("cv.id = :id", { id })
+            .getOne()
+            if(userDetails) return userDetails
+            throw new UserDetailsNotFoundError(UserDetailsNotFoundError.defaultMessage)
+        } catch(e) {
+            console.error(e);
+            return null
+        }
+
+    }
+    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+
 }
