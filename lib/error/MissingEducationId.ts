01b06b1ea3f8c4dd7eb05957d2e662d18d802459
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:24:56 +0300

    added default message

--- lib/error/MissingEducationId.ts
+++ lib/error/MissingEducationId.ts
@@ -1,4 +1,5 @@
 export default class MissingEducationId extends Error {
+    static defaultMessage: string = "Missing education id"
     constructor(message: string){
         super(message)
         this.name = "MissingEducationId"
