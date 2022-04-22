1d09a33542a32a68dbb4862e8ce393d11922ee4b
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:27:41 +0300

    added default message

--- lib/error/PersonalSkillNotFoundError.ts
+++ lib/error/PersonalSkillNotFoundError.ts
@@ -1,4 +1,5 @@
 export default class PersonalSkillNotFoundError extends Error {
+    static defaultMessage: string = "Personal skill not found"
     constructor(message: string){
         super(message)
         this.name = "PersonalSkillNotFoundError"
