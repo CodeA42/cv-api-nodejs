b3517e2b9e1478f0ed04713d3b55af877ca04aef
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-23 00:05:59 +0300

    some more handling

--- lib/db/queries/cv/image/getImageWithId.ts
+++ lib/db/queries/cv/image/getImageWithId.ts
@@ -1,12 +1,23 @@
 import { AppDataSource } from "../../..";
 import ImageNotFoundError from "../../../../error/ImageNotFoundError";
+import MissingImageIdError from "../../../../error/MissingImageIdError";
 import Image from "../../../Entities/Image.Entity";
 
-export default async function getImageWithId(id: string): Promise<Image> {
-    const image: Image = await AppDataSource.manager
-    .createQueryBuilder(Image, "image").select("image").where("image.id = :id", { id }).getOne()
-    if(image) {
-        return image
+export default async function getImageWithId(id: string): Promise<Image | null> {
+    if(id){
+        try {
+            const image: Image = await AppDataSource
+            .manager
+            .createQueryBuilder(Image, "image")
+            .select("image")
+            .where("image.id = :id", { id })
+            .getOne()
+            if(image) return image
+            throw new ImageNotFoundError(ImageNotFoundError.defaultMessage)
+        } catch(e) {
+            console.error(e);
+            return null
+        }
     }
-    throw new ImageNotFoundError("Image not found")
+    throw new MissingImageIdError(MissingImageIdError.defaultMessage)
 }
