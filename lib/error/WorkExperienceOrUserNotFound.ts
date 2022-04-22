98811d08b84e4bdf0ff5737e4db1b21403b729e0
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:27:36 +0300

    added default message

--- lib/error/WorkExperienceOrUserNotFound.ts
+++ lib/error/WorkExperienceOrUserNotFound.ts
@@ -1,4 +1,5 @@
 export default class WorkExperienceOrUserNotFound extends Error {
+    static defaultMessage: string = "Work experience or user not found"
     constructor(message: string){
         super(message)
         this.name = "WorkExperienceOrUserNotFound"
