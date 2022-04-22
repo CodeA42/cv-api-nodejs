f2bc8106fb9dfe9740d2827647b832aadba9a1f9
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:25:46 +0300

    new error class

---
+++ lib/error/PersonalSkillOrUserNotFoundError.ts
@@ -0,0 +1,7 @@
+export default class PersonalSkillOrUserNotFoundError extends Error {
+    static defaultMessage: string = "Personal skill or user not found"
+    constructor(message: string){
+        super(message)
+        this.name = "PersonalSkillOrUserNotFoundError"
+    }
+}
