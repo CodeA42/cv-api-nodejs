8bef0a233038a17d4fc32a22bb7870dfd8785959
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:24:28 +0300

    added default message

--- lib/error/CvOrUserNotFoundError.ts
+++ lib/error/CvOrUserNotFoundError.ts
@@ -1,4 +1,5 @@
 export default class CvOrUserNotFoundError extends Error {
+    static defaultMessage: string = "Cv or user not found"
     constructor(message: string){
         super(message)
         this.name = "CvOrUserNotFoundError"
