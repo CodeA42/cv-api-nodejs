52a1aafdb140c4814adb17787a9943f1d127d42e
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-22 18:28:07 +0300

    some more errors

--- lib/error/MissingPersonalSkillIdError.ts
+++ lib/error/MissingPersonalSkillIdError.ts
@@ -1,4 +1,5 @@
 export default class MissingPersonalSkillIdError extends Error {
+    static defaultMessage: string = "Missing personal skill id"
     constructor(message: string){
         super(message)
         this.name = "MissingPersonalSkillIdError"
