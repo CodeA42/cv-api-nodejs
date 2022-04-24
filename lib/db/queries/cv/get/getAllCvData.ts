f11ebf5e473a2d3499f8134da55c7e9cba8223e1
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-24 10:01:08 +0300

    no need to sue the repo thing

--- lib/db/queries/cv/get/getAllCvData.ts
+++ lib/db/queries/cv/get/getAllCvData.ts
@@ -1,16 +1,23 @@
-import { AppDataSource } from "../../..";
-import Cv from "../../../Entities/Cv.Entity";
+import { AppDataSource } from "../../.."
+import CvNotFoundError from "../../../../error/CvNotFoundError"
+import MissingCvIdError from "../../../../error/MissingCvIdError"
+import Cv from "../../../Entities/Cv.Entity"
 
-export default async function getAllCvData(id: string): Promise<Cv> {
-    if(id !== undefined){
+export default async function getAllCvData(id: string): Promise<Cv | null> {
+    if(id){
         try{
-            const cvRepo = AppDataSource.getRepository(Cv)
-            return await cvRepo.findOneBy({id})
-            
+            try {
+                const cv: Cv = await AppDataSource.manager.findOne(Cv, {where: {id}})
+                if(cv) return cv
+                throw new CvNotFoundError(CvNotFoundError.defaultMessage)
+            } catch(e) {
+                console.error(e)
+                return null
+            }
         } catch(e) {
             console.error(e)
-            return undefined
+            return null
         }
     }
-    return undefined
+    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
 }
