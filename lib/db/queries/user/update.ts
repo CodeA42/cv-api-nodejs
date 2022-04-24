0244866a4158850272b766ccbb43f078ecf3dd43
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-24 10:35:26 +0300

    added check for user id added check for user after search query

--- lib/db/queries/user/update.ts
+++ lib/db/queries/user/update.ts
@@ -1,18 +1,23 @@
 import { AppDataSource } from "../..";
 import { UserData } from "../../../common/types";
+import MissingUserIdError from "../../../error/MissingUserIdError";
+import UserNotFoundError from "../../../error/UserNotFoundError";
 import User from "../../Entities/User.Entity";
 
-export default async function updateUserData(userData: UserData){
-    const userRepo = AppDataSource.getRepository(User);
-    const user = await userRepo.findOneBy({id: userData.id})
-
-    if(userData.firstName)              user.firstName = userData.firstName
-    if(userData.lastName)               user.lastName = userData.lastName
-
-    if(userData.details.address)        user.details.address = userData.details.address
-    if(userData.details.town)           user.details.town = userData.details.town
-    if(userData.details.country)        user.details.country = userData.details.country
-    if(userData.details.phoneNumber)    user.details.phoneNumber = userData.details.phoneNumber
-
-    return await userRepo.save(user)
+export default async function updateUserData(userData: UserData): Promise<User | null>{
+    if(userData.id){
+        const user = await AppDataSource.manager.findOneBy(User, {id: userData.id})
+        if(!user) throw new UserNotFoundError(UserNotFoundError.defaultMessage)
+    
+        if(userData.firstName)              user.firstName = userData.firstName
+        if(userData.lastName)               user.lastName = userData.lastName
+    
+        if(userData.details.address)        user.details.address = userData.details.address
+        if(userData.details.town)           user.details.town = userData.details.town
+        if(userData.details.country)        user.details.country = userData.details.country
+        if(userData.details.phoneNumber)    user.details.phoneNumber = userData.details.phoneNumber
+    
+        return await AppDataSource.manager.save(user)
+    }
+    throw new MissingUserIdError(MissingUserIdError.defaultMessage)
 }
