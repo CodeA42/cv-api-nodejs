7dd21c8e14daf7e00b35cfaa0f0862134bae594c
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-24 10:33:37 +0300

    added check for cvId

--- lib/db/queries/cv/update/updateImage.ts
+++ lib/db/queries/cv/update/updateImage.ts
@@ -1,22 +1,25 @@
-import { AppDataSource } from "../../..";
-import CvNotFoundError from "../../../../error/CvNotFoundError";
-import Cv from "../../../Entities/Cv.Entity";
-import Image from "../../../Entities/Image.Entity";
-import UserDetails from "../../../Entities/UserDetails.Entity";
-import getCvDetailsWithCvId from "../get/getCvDetailsWithCvId";
-import getCvWithDetails from "../get/getCvWithDetails";
+import { AppDataSource } from "../../.."
+import CvNotFoundError from "../../../../error/CvNotFoundError"
+import MissingCvIdError from "../../../../error/MissingCvIdError"
+import Cv from "../../../Entities/Cv.Entity"
+import Image from "../../../Entities/Image.Entity"
+import UserDetails from "../../../Entities/UserDetails.Entity"
+import getCvDetailsWithCvId from "../get/getCvDetailsWithCvId"
+import getCvWithDetails from "../get/getCvWithDetails"
 
 export default async function updateImage(cvId: string, avatar: string){
-    let details: UserDetails = await getCvDetailsWithCvId(cvId)
+    if(cvId){
+        let details: UserDetails = await getCvDetailsWithCvId(cvId)
+        
+        if(!details) details = new UserDetails()
+        if(!details.image) details.image = new Image()
+        details.image.avatar = avatar
+        
+        if(details.id) return await AppDataSource.manager.save(details)
     
-    if(!details) details = new UserDetails()
-    if(!details.image) details.image = new Image()
-    details.image.avatar = avatar
-    
-    if(details.id){
-        return await AppDataSource.manager.save(details)
+        const cv: Cv = await getCvWithDetails(cvId)
+        cv.details = details
+        return await AppDataSource.manager.save(cv)
     }
-    const cv: Cv = await getCvWithDetails(cvId)
-    cv.details = details
-    return await AppDataSource.manager.save(cv)
+    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
 }
