6adf1792fe69fceffcd24a404508523706f51797
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-24 10:32:16 +0300

    there was an extra try catch block

--- lib/db/queries/cv/get/getAllCvData.ts
+++ lib/db/queries/cv/get/getAllCvData.ts
@@ -5,15 +5,10 @@ import Cv from "../../../Entities/Cv.Entity"
 
 export default async function getAllCvData(id: string): Promise<Cv | null> {
     if(id){
-        try{
-            try {
-                const cv: Cv = await AppDataSource.manager.findOne(Cv, {where: {id}})
-                if(cv) return cv
-                throw new CvNotFoundError(CvNotFoundError.defaultMessage)
-            } catch(e) {
-                console.error(e)
-                return null
-            }
+        try {
+            const cv: Cv = await AppDataSource.manager.findOne(Cv, {where: {id}})
+            if(cv) return cv
+            throw new CvNotFoundError(CvNotFoundError.defaultMessage)
         } catch(e) {
             console.error(e)
             return null
