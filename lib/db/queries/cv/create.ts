32d743048bf00cca7eebb413fe5b13b85ee69275
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-24 10:34:03 +0300

    added check for a cv

--- lib/db/queries/cv/create.ts
+++ lib/db/queries/cv/create.ts
@@ -1,7 +1,10 @@
 import { AppDataSource } from "../..";
+import CvNotFoundError from "../../../error/CvNotFoundError";
 import Cv from "../../Entities/Cv.Entity";
 
-export default async function createNewCv(cv: Cv): Promise<Cv>{
-    const cvRepo = AppDataSource.getRepository(Cv);
-    return await cvRepo.save(cv);
+export default async function createNewCv(cv: Cv): Promise<Cv | null>{
+    if(cv){
+        return await AppDataSource.manager.save(cv);
+    }
+    throw new CvNotFoundError(CvNotFoundError.defaultMessage)
 }
