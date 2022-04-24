e72d6562d97421444f8709b09488c440ffaa29f9
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-24 10:35:37 +0300

    user not found error

---
+++ lib/error/UserNotFoundError.ts
@@ -0,0 +1,7 @@
+export default class UserNotFoundError extends Error {
+    static defaultMessage: string = "Missing education id"
+    constructor(message: string){
+        super(message)
+        this.name = "UserNotFoundError"
+    }
+}
