9b78438943a93377ee545e24a841496d91119ff5
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-24 10:33:50 +0300

    added check for id

--- lib/db/queries/user/create.ts
+++ lib/db/queries/user/create.ts
@@ -1,9 +1,13 @@
 import { AppDataSource } from "../..";
+import MissingUserIdError from "../../../error/MissingUserIdError";
 import User from "../../Entities/User.Entity";
 
-export default async function createUser(id: string){
-    const user = new User();
-    user.id = id;
-
-    return await AppDataSource.manager.save(user);
+export default async function createUser(id: string): Promise<User>{
+    if(id){
+        const user = new User();
+        user.id = id;
+    
+        return await AppDataSource.manager.save(user);
+    }
+    throw new MissingUserIdError(MissingUserIdError.defaultMessage)
 }
