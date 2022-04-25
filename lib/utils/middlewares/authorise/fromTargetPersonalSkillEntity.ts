6cf6a39ef4d6ee1ed4fd2721f127fe512b5870e7
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:52:38 +0300

    better if check

--- lib/utils/middlewares/authorise/fromTargetPersonalSkillEntity.ts
+++ lib/utils/middlewares/authorise/fromTargetPersonalSkillEntity.ts
@@ -5,16 +5,15 @@ import ImageOrUserNotFoundError from "../../../error/ImageOrUserNotFoundError"
 import MissingPersonalSkillIdError from "../../../error/MissingPersonalSkillIdError"
 
 export default async function fromTargetPersonalSkillEntity(req: Request, res: Response) {
-    if(req.params.id){
-        try {
-            const user: User = await findUserIdFromImageId(req.params.id)
-            if(user) return user.id
-        } catch(e) {
-            if(e instanceof ImageOrUserNotFoundError){
-                return res.status(404).json(e.message)
-            }
+    if(!req.params.id) throw new MissingPersonalSkillIdError(MissingPersonalSkillIdError.defaultMessage)
+    
+    try {
+        const user: User = await findUserIdFromImageId(req.params.id)
+        if(user) return user.id
+    } catch(e) {
+        if(e instanceof ImageOrUserNotFoundError){
+            return res.status(404).json(e.message)
         }
-        return null
     }
-    throw new MissingPersonalSkillIdError(MissingPersonalSkillIdError.defaultMessage)
+    return null
 }
