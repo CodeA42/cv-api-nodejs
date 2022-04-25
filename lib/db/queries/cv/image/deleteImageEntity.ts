9d8042946607c85b51c6addd47369f50d8dc2e45
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:23:26 +0300

    better if check

--- lib/db/queries/cv/image/deleteImageEntity.ts
+++ lib/db/queries/cv/image/deleteImageEntity.ts
@@ -3,8 +3,7 @@ import MissingImageIdError from "../../../../error/MissingImageIdError";
 import Image from "../../../Entities/Image.Entity";
 
 export default async function deleteImageEntity(id: string) {
-    if(id){
-        return await AppDataSource.manager.delete(Image, { id })
-    }
-    throw new MissingImageIdError(MissingImageIdError.defaultMessage)
+    if(!id) throw new MissingImageIdError(MissingImageIdError.defaultMessage)
+    
+    return await AppDataSource.manager.delete(Image, { id })
 }
