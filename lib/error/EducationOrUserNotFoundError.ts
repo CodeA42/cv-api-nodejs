d8ad9fa291ad07b52b7eb7f003e4e82908c68f4d
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:24:23 +0300

    renamed

--- lib/error/EducationOrUserNotFoundError.ts
+++ lib/error/EducationOrUserNotFoundError.ts
@@ -1,7 +1,7 @@
-export default class EducationOrUserNotFound extends Error {
+export default class EducationOrUserNotFoundError extends Error {
     static defaultMessage: string = "Education or user not found"
     constructor(message: string){
         super(message)
-        this.name = "EducationOrUserNotFound"
+        this.name = "EducationOrUserNotFoundError"
     }
 }
