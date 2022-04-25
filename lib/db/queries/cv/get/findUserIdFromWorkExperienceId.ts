ebd5069dc75b4925c7f3a5caca759d1a34d1c2a0
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:23:04 +0300

    better if check renamed the error message import

--- lib/db/queries/cv/get/findUserIdFromWorkExperienceId.ts
+++ lib/db/queries/cv/get/findUserIdFromWorkExperienceId.ts
@@ -1,26 +1,25 @@
 import { AppDataSource } from "../../.."
-import MissingWorkExperienceId from "../../../../error/MissingWorkExperienceId"
-import WorkExperienceOrUserNotFound from "../../../../error/WorkExperienceOrUserNotFound"
+import MissingWorkExperienceIdError from "../../../../error/MissingWorkExperienceIdError"
+import WorkExperienceOrUserNotFoundError from "../../../../error/WorkExperienceOrUserNotFoundError"
 import User from "../../../Entities/User.Entity"
 
 export default async function findUserIdFromWorkExperienceId(id: string): Promise<User | null> {
-    if(id){
-        try{
-            const user: User = await AppDataSource
-            .manager
-            .createQueryBuilder()
-            .select("user.id")
-            .from(User, "user")
-            .leftJoin("user.cvs", "cv")
-            .leftJoin("cv.workExperience","workExperience")
-            .where("workExperience.id = :id", {id})
-            .getOne()
-            if(user) return user
-            throw new WorkExperienceOrUserNotFound(WorkExperienceOrUserNotFound.defaultMessage)
-        } catch(e) {
-            console.error(e)
-            return null
-        }
+    if(!id) throw new MissingWorkExperienceIdError(MissingWorkExperienceIdError.defaultMessage)
+    
+    try{
+        const user: User = await AppDataSource
+        .manager
+        .createQueryBuilder()
+        .select("user.id")
+        .from(User, "user")
+        .leftJoin("user.cvs", "cv")
+        .leftJoin("cv.workExperience","workExperience")
+        .where("workExperience.id = :id", {id})
+        .getOne()
+        if(user) return user
+        throw new WorkExperienceOrUserNotFoundError(WorkExperienceOrUserNotFoundError.defaultMessage)
+    } catch(e) {
+        console.error(e)
+        return null
     }
-    throw new MissingWorkExperienceId(MissingWorkExperienceId.defaultMessage)
 }
