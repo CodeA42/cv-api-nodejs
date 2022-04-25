d6f4ed02415220ef783ee4c09a335d64ed60c0f3
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:52:43 +0300

    better if check

--- lib/utils/middlewares/authorise/fromTargetWorkExperienceEntity.ts
+++ lib/utils/middlewares/authorise/fromTargetWorkExperienceEntity.ts
@@ -5,16 +5,15 @@ import MissingWorkExperienceId from "../../../error/MissingWorkExperienceIdError
 import WorkExperienceOrUserNotFoundError from "../../../error/WorkExperienceOrUserNotFoundError"
 
 export default async function fromTargetWorkExperienceEntity(req: Request, res: Response) {
-    if(req.params.id){
-        try {
-            const user: User = await findUserIdFromWorkExperienceId(req.params.id)
-            if(user) return user.id
-        } catch(e) {
-            if(e instanceof WorkExperienceOrUserNotFoundError){
-                return res.status(404).json(e.message)
-            }
+    if(req.params.id) throw new MissingWorkExperienceId(MissingWorkExperienceId.defaultMessage)
+
+    try {
+        const user: User = await findUserIdFromWorkExperienceId(req.params.id)
+        if(user) return user.id
+    } catch(e) {
+        if(e instanceof WorkExperienceOrUserNotFoundError){
+            return res.status(404).json(e.message)
         }
-        return null
     }
-    throw new MissingWorkExperienceId(MissingWorkExperienceId.defaultMessage)
+    return null    
 }
