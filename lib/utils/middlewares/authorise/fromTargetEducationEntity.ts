40f93a6ea0d46d500bab7553a63d5c51aa2c7c96
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:29:52 +0300

    Was sending the error name to the user was returning undefined instead of null

--- lib/utils/middlewares/authorise/fromTargetEducationEntity.ts
+++ lib/utils/middlewares/authorise/fromTargetEducationEntity.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from "express";
-import User from "../../../db/Entities/User.Entity";
-import findUserIdFromEducationId from "../../../db/queries/cv/get/findUserIdFromEducationId";
-import EducationOrUserNotFound from "../../../error/EducationOrUserNotFound";
-import MissingEducationId from "../../../error/MissingEducationId";
+import { Request, Response } from "express"
+import User from "../../../db/Entities/User.Entity"
+import findUserIdFromEducationId from "../../../db/queries/cv/get/findUserIdFromEducationId"
+import EducationOrUserNotFoundError from "../../../error/EducationOrUserNotFoundError"
+import MissingEducationId from "../../../error/MissingEducationIdError"
 
 export default async function fromTargetEducationEntity(req: Request, res: Response) {
     if(req.params.id){
@@ -10,11 +10,11 @@ export default async function fromTargetEducationEntity(req: Request, res: Respo
             const user: User = await findUserIdFromEducationId(req.params.id)
             if(user) return user.id
         } catch(e) {
-            if(e instanceof EducationOrUserNotFound) {
-                return res.status(404).json({name: e.name})
-            }            
+            if(e instanceof EducationOrUserNotFoundError) {
+                return res.status(404).json(EducationOrUserNotFoundError.defaultMessage)
+            }
         }
-        return undefined
+        return null
     }
     throw new MissingEducationId(MissingEducationId.defaultMessage);
 }
