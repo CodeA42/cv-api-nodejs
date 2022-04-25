0b9ef370c6ad058f9539e93d9bf55466453a5b9b
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:25:53 +0300

    Added some error catching

--- lib/routes/cv/workExperience/delete.ts
+++ lib/routes/cv/workExperience/delete.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express"
 import deleteWorkExperienceEntity from "../../../db/queries/cv/delete/deleteWorkExperienceEntity"
+import MissingWorkExperienceIdError from "../../../error/MissingWorkExperienceIdError"
 
 export default async function deleteWorkExperience(req: Request, res: Response) {
     try{
         await deleteWorkExperienceEntity(req.params.id)
         return res.sendStatus(200)
     } catch(e) {
+        if(e instanceof MissingWorkExperienceIdError){
+            return res.status(400).json(MissingWorkExperienceIdError.defaultMessage)
+        }
         console.error(e)
         return res.sendStatus(500)
     }
