b46184394f3fce8b1004d76e5b58745b2a837684
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:50:55 +0300

    sending the actual message is better than sending default message

--- lib/routes/cv/update.ts
+++ lib/routes/cv/update.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import Cv from "../../db/Entities/Cv.Entity"
 import getAllCvData from "../../db/queries/cv/get/getAllCvData"
 import updateCvData from "../../db/queries/cv/update"
+import MissingCvIdError from "../../error/MissingCvIdError"
 
 export default async function updateCv(req: Request, res: Response) {
     try{
@@ -9,6 +10,8 @@ export default async function updateCv(req: Request, res: Response) {
         const response = await updateCvData(cv, res.locals.cvData)
         return res.status(200).json(response)
     } catch(e){
+        if(e instanceof MissingCvIdError) return res.status(400).json(e.message)
+        
         console.log(e)
         return res.sendStatus(500)
     }
