9a2e90c502963e5761e950d37c8ba38b7ab81f3c
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:23:09 +0300

    better if check

--- lib/db/queries/cv/get/getAllCvData.ts
+++ lib/db/queries/cv/get/getAllCvData.ts
@@ -4,15 +4,14 @@ import MissingCvIdError from "../../../../error/MissingCvIdError"
 import Cv from "../../../Entities/Cv.Entity"
 
 export default async function getAllCvData(id: string): Promise<Cv | null> {
-    if(id){
-        try {
-            const cv: Cv = await AppDataSource.manager.findOne(Cv, {where: {id}})
-            if(cv) return cv
-            throw new CvNotFoundError(CvNotFoundError.defaultMessage)
-        } catch(e) {
-            console.error(e)
-            return null
-        }
+    if(!id) throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+    
+    try {
+        const cv: Cv = await AppDataSource.manager.findOne(Cv, {where: {id}})
+        if(cv) return cv
+        throw new CvNotFoundError(CvNotFoundError.defaultMessage)
+    } catch(e) {
+        console.error(e)
+        return null
     }
-    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
 }
