3720a0ce83ee234e5dcbc76a6640317496ddd8f6
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:23:22 +0300

    better if check

--- lib/db/queries/cv/get/getCvWithDetails.ts
+++ lib/db/queries/cv/get/getCvWithDetails.ts
@@ -1,24 +1,23 @@
-import { AppDataSource } from "../../..";
-import CvNotFoundError from "../../../../error/CvNotFoundError";
-import MissingCvIdError from "../../../../error/MissingCvIdError";
-import Cv from "../../../Entities/Cv.Entity";
+import { AppDataSource } from "../../.."
+import CvNotFoundError from "../../../../error/CvNotFoundError"
+import MissingCvIdError from "../../../../error/MissingCvIdError"
+import Cv from "../../../Entities/Cv.Entity"
 
 export default async function getCvWithDetails(id: string): Promise<Cv | null> {
-    if(id){
-        try{
-            const cv: Cv = await AppDataSource
-            .manager
-            .createQueryBuilder()
-            .select("cv")
-            .from(Cv, "cv")
-            .leftJoinAndSelect("cv.details","details")
-            .getOne()
-            if(cv) return cv
-            throw new CvNotFoundError(CvNotFoundError.defaultMessage) 
-        } catch(e) {
-            console.error(e);
-            return null
-        }
+    if(!id) throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+    
+    try{
+        const cv: Cv = await AppDataSource
+        .manager
+        .createQueryBuilder()
+        .select("cv")
+        .from(Cv, "cv")
+        .leftJoinAndSelect("cv.details","details")
+        .getOne()
+        if(cv) return cv
+        throw new CvNotFoundError(CvNotFoundError.defaultMessage) 
+    } catch(e) {
+        console.error(e);
+        return null
     }
-    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
 }
