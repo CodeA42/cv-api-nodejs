69bd4b172c90a5c28fae101c169c39ff9a6af2a4
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:25:41 +0300

    Added some error catching

--- lib/routes/cv/education/delete.ts
+++ lib/routes/cv/education/delete.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express"
 import deleteEducationEntity from "../../../db/queries/cv/delete/deleteEducationEntity"
+import MissingEducationIdError from "../../../error/MissingEducationIdError"
 
 export default async function deleteEducation(req: Request, res: Response) {
     try{
         await deleteEducationEntity(req.params.id)
         return res.sendStatus(200)
     } catch(e) {
+        if(e instanceof MissingEducationIdError) {
+            return res.status(400).json(MissingEducationIdError.defaultMessage)
+        }
         console.error(e)
         return res.sendStatus(500)
     }
