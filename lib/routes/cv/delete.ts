aab96ec55171d245993c4f7482ee6fbd7c532d47
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:26:04 +0300

    Added some error catching

--- lib/routes/cv/delete.ts
+++ lib/routes/cv/delete.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express"
 import deleteCv from "../../db/queries/cv/delete/deleteCv"
+import MissingCvIdError from "../../error/MissingCvIdError"
 
 export default async function deleteUserCv(req: Request, res: Response) {
     try{
         await deleteCv(req.params.id)
         return res.sendStatus(200)
     } catch(e) {
+        if(e instanceof MissingCvIdError){
+            return res.status(400).json(MissingCvIdError.defaultMessage)
+        }
         console.error(e)
         return res.sendStatus(500)
     }
