442a9f56db4e0791aaf71f1bed5816f04c26f933
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:25:59 +0300

    Added some error catching

--- lib/routes/cv/create.ts
+++ lib/routes/cv/create.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import createNewCv from "../../db/queries/cv/create"
+import CvNotFoundError from "../../error/CvNotFoundError"
 
 export default async function createCv(req: Request, res: Response) {
     try{
@@ -7,6 +8,9 @@ export default async function createCv(req: Request, res: Response) {
 
         return res.status(200).json(cv.id)
     } catch(e){
+        if(e instanceof CvNotFoundError) {
+            return res.send(400).json(CvNotFoundError.defaultMessage)
+        }
         console.error(e)
         return res.sendStatus(500)
     }
