d4ca06b40f5f10d88d651cf73a0cdda9d8631dde
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:30:14 +0300

    will be using file storage from now

--- lib/utils/middlewares/multer.ts
+++ lib/utils/middlewares/multer.ts
@@ -2,14 +2,12 @@ import * as multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
 import * as path from "path";
 
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, 'uploads/')
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, uuidv4() + path.extname(file.originalname)) //Appending extension
-//     }
-// })
-
-var storage = multer.memoryStorage()
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, uuidv4() + path.extname(file.originalname)) //Appending extension
+    }
+})
 export default multer({storage: storage});
