618d3e41c0e9b3b8866d9ca97689d210a5181ba4
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:23:18 +0300

    better if check

--- lib/db/queries/cv/get/getImageEntityWithCvId.ts
+++ lib/db/queries/cv/get/getImageEntityWithCvId.ts
@@ -4,23 +4,22 @@ import MissingCvIdError from "../../../../error/MissingCvIdError";
 import Image from "../../../Entities/Image.Entity";
 
 export default async function getImageEntityWithCvId(id: String): Promise<Image | null> {
-    if(id){
-        try {
-            const image: Image = await AppDataSource
-            .manager
-            .createQueryBuilder()
-            .select("image")
-            .from(Image, "image")
-            .leftJoin("image.userDetails", "userDetails")
-            .leftJoin("userDetails.cv", "cv")
-            .where("cv.id = :id", { id })
-            .getOne()
-            if(image) return image
-            throw new ImageNotFoundError(ImageNotFoundError.defaultMessage)
-        } catch(e){
-            console.error(e);
-            return null
-        }
+    if(!id) throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+    
+    try {
+        const image: Image = await AppDataSource
+        .manager
+        .createQueryBuilder()
+        .select("image")
+        .from(Image, "image")
+        .leftJoin("image.userDetails", "userDetails")
+        .leftJoin("userDetails.cv", "cv")
+        .where("cv.id = :id", { id })
+        .getOne()
+        if(image) return image
+        throw new ImageNotFoundError(ImageNotFoundError.defaultMessage)
+    } catch(e){
+        console.error(e);
+        return null
     }
-    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
 }
