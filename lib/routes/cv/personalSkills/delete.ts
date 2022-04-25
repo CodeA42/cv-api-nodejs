55aff8e42f304aaefee3472f721aa6e4e210e5d6
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:25:44 +0300

    Added some error catching

--- lib/routes/cv/personalSkills/delete.ts
+++ lib/routes/cv/personalSkills/delete.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express"
 import deletePersonalSkillEntity from "../../../db/queries/cv/delete/deletePersonalSkillEntity"
+import MissingPersonalSkillIdError from "../../../error/MissingPersonalSkillIdError"
 
 export default async function deletePersonalSkill(req: Request, res: Response) {
     try{
         await deletePersonalSkillEntity(req.params.id)
         return res.sendStatus(200)
     } catch(e) {
+        if(e instanceof MissingPersonalSkillIdError){
+            return res.status(400).json(MissingPersonalSkillIdError.defaultMessage)
+        }
         console.error(e)
         return res.sendStatus(500)
     }
