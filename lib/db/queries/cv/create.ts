d5277fda0bb3b71aa8e8eb9060fd375ed90971cb
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:23:41 +0300

    better if check

--- lib/db/queries/cv/create.ts
+++ lib/db/queries/cv/create.ts
@@ -1,10 +1,9 @@
-import { AppDataSource } from "../..";
-import CvNotFoundError from "../../../error/CvNotFoundError";
-import Cv from "../../Entities/Cv.Entity";
+import { AppDataSource } from "../.."
+import CvNotFoundError from "../../../error/CvNotFoundError"
+import Cv from "../../Entities/Cv.Entity"
 
 export default async function createNewCv(cv: Cv): Promise<Cv | null>{
-    if(cv){
-        return await AppDataSource.manager.save(cv);
-    }
-    throw new CvNotFoundError(CvNotFoundError.defaultMessage)
+    if(!cv) throw new CvNotFoundError(CvNotFoundError.defaultMessage)
+    
+    return await AppDataSource.manager.save(cv)
 }
