37a8aca6ad29e00785774f9d1038e655ba220a83
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:22:45 +0300

    better if check renamed the error message

--- lib/db/queries/cv/get/findUserIdFromEducationId.ts
+++ lib/db/queries/cv/get/findUserIdFromEducationId.ts
@@ -1,26 +1,25 @@
 import { AppDataSource } from "../../.."
-import EducationOrUserNotFound from "../../../../error/EducationOrUserNotFound"
-import MissingEducationId from "../../../../error/MissingEducationId"
+import EducationOrUserNotFoundError from "../../../../error/EducationOrUserNotFoundError"
+import MissingEducationIdError from "../../../../error/MissingEducationIdError"
 import User from "../../../Entities/User.Entity"
 
 export default async function findUserIdFromEducationId(id: string): Promise<User | null> {
-    if(id){
-        try{
-            const user: User = await AppDataSource
-            .manager
-            .createQueryBuilder()
-            .select("user.id")
-            .from(User, "user")
-            .leftJoin("user.cvs", "cv")
-            .leftJoin("cv.education","education")
-            .where("education.id = :id", {id})
-            .getOne()
-            if(user) return user
-            throw new EducationOrUserNotFound(EducationOrUserNotFound.defaultMessage)
-        } catch(e) {
-            console.error(e)
-            return null
-        }
+    if(!id) throw new MissingEducationIdError(MissingEducationIdError.defaultMessage)
+    
+    try{
+        const user: User = await AppDataSource
+        .manager
+        .createQueryBuilder()
+        .select("user.id")
+        .from(User, "user")
+        .leftJoin("user.cvs", "cv")
+        .leftJoin("cv.education","education")
+        .where("education.id = :id", {id})
+        .getOne()
+        if(user) return user
+        throw new EducationOrUserNotFoundError(EducationOrUserNotFoundError.defaultMessage)
+    } catch(e) {
+        console.error(e)
+        return null
     }
-    throw new MissingEducationId(MissingEducationId.defaultMessage)
 }
