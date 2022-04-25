9e194f1c15492ad1e26e7c5160900e0d70520737
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:23:32 +0300

    better if check

--- lib/db/queries/cv/image/findUserIdFromImageId.ts
+++ lib/db/queries/cv/image/findUserIdFromImageId.ts
@@ -4,24 +4,23 @@ import MissingImageIdError from "../../../../error/MissingImageIdError";
 import User from "../../../Entities/User.Entity";
 
 export default async function findUserIdFromImageId(id: string): Promise<User | null> {
-    if(id) {
-        try{
-            const user: User = await AppDataSource
-            .manager
-            .createQueryBuilder()
-            .select("user.id")
-            .from(User, "user")
-            .leftJoin("user.cvs", "cv")
-            .leftJoin("cv.details","details")
-            .leftJoin("details.image", "image")
-            .where("image.id = :id", { id })
-            .getOne()
-            if(user) return user
-            throw new ImageOrUserNotFoundError(ImageOrUserNotFoundError.defaultMessage)
-        } catch(e) {
-            console.error(e);
-            return null
-        }
+    if(!id) throw new MissingImageIdError(MissingImageIdError.defaultMessage)
+    
+    try{
+        const user: User = await AppDataSource
+        .manager
+        .createQueryBuilder()
+        .select("user.id")
+        .from(User, "user")
+        .leftJoin("user.cvs", "cv")
+        .leftJoin("cv.details","details")
+        .leftJoin("details.image", "image")
+        .where("image.id = :id", { id })
+        .getOne()
+        if(user) return user
+        throw new ImageOrUserNotFoundError(ImageOrUserNotFoundError.defaultMessage)
+    } catch(e) {
+        console.error(e);
+        return null
     }
-    throw new MissingImageIdError(MissingImageIdError.defaultMessage)
 }
