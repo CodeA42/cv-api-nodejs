d9c331564d2d93cbca63f79e80a8c33039a9b434
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:23:53 +0300

    better if check

--- lib/db/queries/user/update.ts
+++ lib/db/queries/user/update.ts
@@ -5,19 +5,18 @@ import UserNotFoundError from "../../../error/UserNotFoundError";
 import User from "../../Entities/User.Entity";
 
 export default async function updateUserData(userData: UserData): Promise<User | null>{
-    if(userData.id){
-        const user = await AppDataSource.manager.findOneBy(User, {id: userData.id})
-        if(!user) throw new UserNotFoundError(UserNotFoundError.defaultMessage)
+    if(!userData.id) throw new MissingUserIdError(MissingUserIdError.defaultMessage)
     
-        if(userData.firstName)              user.firstName = userData.firstName
-        if(userData.lastName)               user.lastName = userData.lastName
-    
-        if(userData.details.address)        user.details.address = userData.details.address
-        if(userData.details.town)           user.details.town = userData.details.town
-        if(userData.details.country)        user.details.country = userData.details.country
-        if(userData.details.phoneNumber)    user.details.phoneNumber = userData.details.phoneNumber
-    
-        return await AppDataSource.manager.save(user)
-    }
-    throw new MissingUserIdError(MissingUserIdError.defaultMessage)
+    const user = await AppDataSource.manager.findOneBy(User, {id: userData.id})
+    if(!user) throw new UserNotFoundError(UserNotFoundError.defaultMessage)
+
+    if(userData.firstName)              user.firstName = userData.firstName
+    if(userData.lastName)               user.lastName = userData.lastName
+
+    if(userData.details.address)        user.details.address = userData.details.address
+    if(userData.details.town)           user.details.town = userData.details.town
+    if(userData.details.country)        user.details.country = userData.details.country
+    if(userData.details.phoneNumber)    user.details.phoneNumber = userData.details.phoneNumber
+
+    return await AppDataSource.manager.save(user)
 }
