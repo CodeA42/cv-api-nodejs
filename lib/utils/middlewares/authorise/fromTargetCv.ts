beadd8cbc6ca45d749ec8ff08f907203e693a4ad
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:51:57 +0300

    better if check

--- lib/utils/middlewares/authorise/fromTargetCv.ts
+++ lib/utils/middlewares/authorise/fromTargetCv.ts
@@ -1,20 +1,19 @@
-import { Request, Response } from "express";
-import User from "../../../db/Entities/User.Entity";
-import getUserIdFromCvId from "../../../db/queries/user/getUserIdByCvId";
-import CvOrUserNotFoundError from "../../../error/CvOrUserNotFoundError";
-import MissingCvIdError from "../../../error/MissingCvIdError";
+import { Request, Response } from "express"
+import User from "../../../db/Entities/User.Entity"
+import getUserIdFromCvId from "../../../db/queries/user/getUserIdByCvId"
+import CvOrUserNotFoundError from "../../../error/CvOrUserNotFoundError"
+import MissingCvIdError from "../../../error/MissingCvIdError"
 
 export default async function fromTargetCv(req: Request, res: Response){
-    if(req.params.id){
-        try {
-            const user: User = await getUserIdFromCvId(req.params.id)
-            if(user) return user.id
-        } catch(e) {
-            if(e instanceof CvOrUserNotFoundError) {
-                return res.status(404).json(e.message)
-            }
+    if(!req.params.id) throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+    
+    try {
+        const user: User = await getUserIdFromCvId(req.params.id)
+        if(user) return user.id
+    } catch(e) {
+        if(e instanceof CvOrUserNotFoundError) {
+            return res.status(404).json(e.message)
         }
-        return null
     }
-    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+    return null
 }
