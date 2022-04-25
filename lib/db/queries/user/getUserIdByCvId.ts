55c3ebe834ba22f679b312a1eb82cbaba8ad614d
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:23:49 +0300

    better if check

--- lib/db/queries/user/getUserIdByCvId.ts
+++ lib/db/queries/user/getUserIdByCvId.ts
@@ -1,25 +1,24 @@
-import { AppDataSource } from "../..";
-import CvOrUserNotFoundError from "../../../error/CvOrUserNotFoundError";
-import MissingCvIdError from "../../../error/MissingCvIdError";
-import User from "../../Entities/User.Entity";
+import { AppDataSource } from "../.."
+import CvOrUserNotFoundError from "../../../error/CvOrUserNotFoundError"
+import MissingCvIdError from "../../../error/MissingCvIdError"
+import User from "../../Entities/User.Entity"
 
 export default async function getUserIdFromCvId(id: string): Promise<User | null> {
-    if(id) {
-        try {
-            const user: User = await AppDataSource
-            .manager
-            .createQueryBuilder()
-            .select("user.id")
-            .from(User,"user")
-            .leftJoin("user.cvs", "cv")
-            .where("cv.id = :id", {id})
-            .getOne()
-            if(user) return user
-            throw new CvOrUserNotFoundError(CvOrUserNotFoundError.defaultMessage)
-        } catch(e) {
-            console.error(e)
-            return null
-        }
+    if(!id) throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+    
+    try {
+        const user: User = await AppDataSource
+        .manager
+        .createQueryBuilder()
+        .select("user.id")
+        .from(User,"user")
+        .leftJoin("user.cvs", "cv")
+        .where("cv.id = :id", {id})
+        .getOne()
+        if(user) return user
+        throw new CvOrUserNotFoundError(CvOrUserNotFoundError.defaultMessage)
+    } catch(e) {
+        console.error(e)
+        return null
     }
-    throw new MissingCvIdError(MissingCvIdError.defaultMessage)
 }
