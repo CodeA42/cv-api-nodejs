2701dcc3007e8ec73d664f110784fa09ed65ae06
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-25 09:52:30 +0300

    better if check was seind undefined not null

--- lib/utils/middlewares/authorise/fromTargetImage.ts
+++ lib/utils/middlewares/authorise/fromTargetImage.ts
@@ -1,20 +1,19 @@
-import { Request, Response } from "express";
-import User from "../../../db/Entities/User.Entity";
-import findUserIdFromImageId from "../../../db/queries/cv/image/findUserIdFromImageId";
-import ImageOrUserNotFoundError from "../../../error/ImageOrUserNotFoundError";
-import MissingImageIdError from "../../../error/MissingImageIdError";
+import { Request, Response } from "express"
+import User from "../../../db/Entities/User.Entity"
+import findUserIdFromImageId from "../../../db/queries/cv/image/findUserIdFromImageId"
+import ImageOrUserNotFoundError from "../../../error/ImageOrUserNotFoundError"
+import MissingImageIdError from "../../../error/MissingImageIdError"
 
 export default async function fromTargetImage(req: Request, res: Response) {
-    if(req.params.id){
-        try{
-            const user: User = await findUserIdFromImageId(req.params.id)
-            if(user) return user.id
-        } catch(e) {
-            if(e instanceof ImageOrUserNotFoundError) {
-                return res.status(404).json(ImageOrUserNotFoundError.defaultMessage)
-            }
+    if(!req.params.id) throw new MissingImageIdError(MissingImageIdError.defaultMessage)
+    
+    try{
+        const user: User = await findUserIdFromImageId(req.params.id)
+        if(user) return user.id
+    } catch(e) {
+        if(e instanceof ImageOrUserNotFoundError) {
+            return res.status(404).json(ImageOrUserNotFoundError.defaultMessage)
         }
-        return undefined
     }
-    throw new MissingImageIdError(MissingImageIdError.defaultMessage)
+    return null
 }
