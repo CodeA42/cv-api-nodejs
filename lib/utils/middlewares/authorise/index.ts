a6eb8e2a85df64889ce8968079daff8a797f2b99
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 17:33:44 +0300

    moved a lot of the logic here

--- lib/utils/middlewares/authorise/index.ts
+++ lib/utils/middlewares/authorise/index.ts
@@ -1,22 +1,44 @@
 import { NextFunction, Request, Response } from "express";
+import CvOrUserNotFoundError from "../../../error/CvOrUserNotFoundError";
+import EducationOrUserNotFoundError from "../../../error/EducationOrUserNotFoundError";
+import ImageOrUserNotFoundError from "../../../error/ImageOrUserNotFoundError";
+import MissingCvIdError from "../../../error/MissingCvIdError";
+import MissingEducationIdError from "../../../error/MissingEducationIdError";
+import MissingImageIdError from "../../../error/MissingImageIdError";
+import MissingPersonalSkillIdError from "../../../error/MissingPersonalSkillIdError";
+import MissingWorkExperienceIdError from "../../../error/MissingWorkExperienceIdError";
+import WorkExperienceOrUserNotFoundError from "../../../error/WorkExperienceOrUserNotFoundError";
 
 // type return_type = ReturnType<(req: Request, res: Response, next: NextFunction) => Promise<void>>
 
 export default function authorise(getTarget: Function){
     return async (req: Request, res: Response, next: NextFunction) => {
-        const target: string = await getTarget(req, res)
-        
-        if(!target) {
-            return res.status(401).json('user not found')
-        }
+        try {
+            const target: string = await getTarget(req, res)
 
-        if(target !== res.locals.user.id){
-            if(res.locals.user.admin){
-                next()
-                return
-            } else return res.sendStatus(403)
-        }
-        
-        next()
+            if(!target) return res.status(401).json('user not found')
+            
+            if(target !== res.locals.user.id){
+                if(res.locals.user.admin){
+                    next()
+                    return
+                } else return res.sendStatus(403)
+            }
+
+            next()
+        } catch(e) {
+            if( e instanceof MissingCvIdError ||
+                e instanceof MissingEducationIdError ||
+                e instanceof MissingImageIdError ||
+                e instanceof MissingPersonalSkillIdError ||
+                e instanceof MissingWorkExperienceIdError
+            ) return res.status(400).json(e.message)
+
+            if( e instanceof CvOrUserNotFoundError ||
+                e instanceof EducationOrUserNotFoundError ||
+                e instanceof ImageOrUserNotFoundError || 
+                e instanceof WorkExperienceOrUserNotFoundError
+            ) return res.status(404).json(e.message)
+        }       
     }
 }
