a9c7bc05d703b4c51965916cbf55caf834357ddf
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 15:00:16 +0300

    Prototype should be set manually in ts

--- lib/error/UserNotFoundError.ts
+++ lib/error/UserNotFoundError.ts
@@ -3,5 +3,7 @@ export default class UserNotFoundError extends Error {
     constructor(message: string){
         super(message)
         this.name = "UserNotFoundError"
+
+        Object.setPrototypeOf(this, UserNotFoundError.prototype)
     }
 }
