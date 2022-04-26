73d2214121b37d5a63c8565dfb24403f22bf81f7
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 14:59:57 +0300

    Prototype should be set manually in ts

--- lib/error/MissingCvIdError.ts
+++ lib/error/MissingCvIdError.ts
@@ -3,5 +3,7 @@ export default class MissingCvIdError extends Error {
     constructor(message: string){
         super(message)
         this.name = "MissingCvIdError"
+
+        Object.setPrototypeOf(this, MissingCvIdError.prototype)
     }
 }
