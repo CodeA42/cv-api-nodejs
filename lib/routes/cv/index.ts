d38365b005780e42555a9252c8a9bf7ecb371ee7
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 15:01:56 +0300

    moved authentication to the top of the router

--- lib/routes/cv/index.ts
+++ lib/routes/cv/index.ts
@@ -1,6 +1,4 @@
 import { Router, Request, Response } from "express"
-import authenticate from "../../utils/middlewares/authenticateUser"
-import getAccessToken from "../../utils/middlewares/authenticateUser/getAccessToken"
 import prepCvData from "../../utils/middlewares/prepCvData"
 import updateCv from "./update"
 import createCv from "./create"
@@ -21,13 +19,13 @@ cvRouter.use('/work-experience', workExpericenceRouter)
 cvRouter.use('/personal-skill', personalSkillsRouter)
 cvRouter.use('/image', imageRouter)
 
-cvRouter.get('/:id', authenticate(getAccessToken), authorise(fromTargetCv), getCv)
+cvRouter.get('/:id', authorise(fromTargetCv), getCv)
 
-cvRouter.post('/', authenticate(getAccessToken), authorise(body), prepCvData, createCv)
+cvRouter.post('/', authorise(body), prepCvData, createCv)
 
-cvRouter.put('/', authenticate(getAccessToken), authorise(bodyTargetCv), prepCvData, updateCv)
+cvRouter.put('/', authorise(bodyTargetCv), prepCvData, updateCv)
 
-cvRouter.delete('/:id', authenticate(getAccessToken), authorise(fromTargetCv), deleteCv)
+cvRouter.delete('/:id', authorise(fromTargetCv), deleteCv)
 
 cvRouter.use("*", (req: Request, res: Response) => {
     res.status(404).json(`Invalid request {${req.originalUrl}}`)
