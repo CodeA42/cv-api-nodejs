14380e001ab6e72d443257023e8e9d8bde7bbf37
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 17:33:23 +0300

    simplified will probably remove at some point

--- lib/utils/middlewares/authorise/fromTargetEducationEntity.ts
+++ lib/utils/middlewares/authorise/fromTargetEducationEntity.ts
@@ -1,19 +1,13 @@
 import { Request, Response } from "express"
 import User from "../../../db/Entities/User.Entity"
 import findUserIdFromEducationId from "../../../db/queries/cv/get/findUserIdFromEducationId"
-import EducationOrUserNotFoundError from "../../../error/EducationOrUserNotFoundError"
 import MissingEducationId from "../../../error/MissingEducationIdError"
 
 export default async function fromTargetEducationEntity(req: Request, res: Response) {
-    if(!req.params.id) throw new MissingEducationId(MissingEducationId.defaultMessage)
+    const id: string = req.params.id
+    if(!id) throw new MissingEducationId(MissingEducationId.defaultMessage)
     
-    try{
-        const user: User = await findUserIdFromEducationId(req.params.id)
-        if(user) return user.id
-    } catch(e) {
-        if(e instanceof EducationOrUserNotFoundError) {
-            return res.status(404).json(e.message)
-        }
-    }
+    const user: User = await findUserIdFromEducationId(id)
+    if(user) return user.id
     return null
 }
