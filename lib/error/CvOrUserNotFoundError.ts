a9931546c1949933b9bbad3ebef4618289a8e8ce
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 14:59:43 +0300

    Prototype should be set manually in ts

--- lib/error/CvOrUserNotFoundError.ts
+++ lib/error/CvOrUserNotFoundError.ts
@@ -3,5 +3,7 @@ export default class CvOrUserNotFoundError extends Error {
     constructor(message: string){
         super(message)
         this.name = "CvOrUserNotFoundError"
+
+        Object.setPrototypeOf(this, CvOrUserNotFoundError.prototype)
     }
 }
