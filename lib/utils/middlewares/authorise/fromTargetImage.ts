405346ab1e521f8b174448dbcff4911ceb9c9309
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 17:33:03 +0300

    simplified will probably remove at some point

--- lib/utils/middlewares/authorise/fromTargetImage.ts
+++ lib/utils/middlewares/authorise/fromTargetImage.ts
@@ -1,19 +1,13 @@
 import { Request, Response } from "express"
 import User from "../../../db/Entities/User.Entity"
 import findUserIdFromImageId from "../../../db/queries/cv/image/findUserIdFromImageId"
-import ImageOrUserNotFoundError from "../../../error/ImageOrUserNotFoundError"
 import MissingImageIdError from "../../../error/MissingImageIdError"
 
 export default async function fromTargetImage(req: Request, res: Response) {
-    if(!req.params.id) throw new MissingImageIdError(MissingImageIdError.defaultMessage)
+    const id: string = req.params.id
+    if(!id) throw new MissingImageIdError(MissingImageIdError.defaultMessage)
     
-    try{
-        const user: User = await findUserIdFromImageId(req.params.id)
-        if(user) return user.id
-    } catch(e) {
-        if(e instanceof ImageOrUserNotFoundError) {
-            return res.status(404).json(ImageOrUserNotFoundError.defaultMessage)
-        }
-    }
+    const user: User = await findUserIdFromImageId(id)
+    if(user) return user.id
     return null
 }
