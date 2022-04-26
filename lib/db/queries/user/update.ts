d003cc81e5f7318937023b360f64852a965c6057
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 17:36:55 +0300

    added a check to see if user data even exists and image can now be changed from the form as well

--- lib/db/queries/user/update.ts
+++ lib/db/queries/user/update.ts
@@ -1,8 +1,9 @@
-import { AppDataSource } from "../..";
-import { UserData } from "../../../common/types";
-import MissingUserIdError from "../../../error/MissingUserIdError";
-import UserNotFoundError from "../../../error/UserNotFoundError";
-import User from "../../Entities/User.Entity";
+import { AppDataSource } from "../.."
+import { UserData } from "../../../common/types"
+import MissingUserIdError from "../../../error/MissingUserIdError"
+import UserNotFoundError from "../../../error/UserNotFoundError"
+import User from "../../Entities/User.Entity"
+import UserDetails from "../../Entities/UserDetails.Entity"
 
 export default async function updateUserData(userData: UserData): Promise<User | null>{
     if(!userData.id) throw new MissingUserIdError(MissingUserIdError.defaultMessage)
@@ -13,6 +14,9 @@ export default async function updateUserData(userData: UserData): Promise<User |
     if(userData.firstName)              user.firstName = userData.firstName
     if(userData.lastName)               user.lastName = userData.lastName
 
+    if(!user.details) user.details = new UserDetails()
+
+    if(userData.details.image)          user.details.image = userData.details.image
     if(userData.details.address)        user.details.address = userData.details.address
     if(userData.details.town)           user.details.town = userData.details.town
     if(userData.details.country)        user.details.country = userData.details.country
