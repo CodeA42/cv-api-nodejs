2efa546fdae6a7815111407144963d8b57f8239f
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 17:33:12 +0300

    simplified will probably remove at some point

--- lib/utils/middlewares/authorise/fromTargetWorkExperienceEntity.ts
+++ lib/utils/middlewares/authorise/fromTargetWorkExperienceEntity.ts
@@ -1,19 +1,13 @@
 import { Request, Response } from "express"
 import User from "../../../db/Entities/User.Entity"
 import findUserIdFromWorkExperienceId from "../../../db/queries/cv/get/findUserIdFromWorkExperienceId"
-import MissingWorkExperienceId from "../../../error/MissingWorkExperienceIdError"
-import WorkExperienceOrUserNotFoundError from "../../../error/WorkExperienceOrUserNotFoundError"
+import MissingWorkExperienceIdError from "../../../error/MissingWorkExperienceIdError"
 
 export default async function fromTargetWorkExperienceEntity(req: Request, res: Response) {
-    if(!req.params.id) throw new MissingWorkExperienceId(MissingWorkExperienceId.defaultMessage)
+    const id: string = req.params.id
+    if(!id) throw new MissingWorkExperienceIdError(MissingWorkExperienceIdError.defaultMessage)
 
-    try {
-        const user: User = await findUserIdFromWorkExperienceId(req.params.id)
-        if(user) return user.id
-    } catch(e) {
-        if(e instanceof WorkExperienceOrUserNotFoundError){
-            return res.status(404).json(e.message)
-        }
-    }
+    const user: User = await findUserIdFromWorkExperienceId(id)
+    if(user) return user.id
     return null    
 }
