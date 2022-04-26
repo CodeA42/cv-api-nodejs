835d282544bf3a112a036c3a4831969498febb4e
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 15:00:07 +0300

    Prototype should be set manually in ts

--- lib/error/MissingPersonalSkillIdError.ts
+++ lib/error/MissingPersonalSkillIdError.ts
@@ -3,5 +3,7 @@ export default class MissingPersonalSkillIdError extends Error {
     constructor(message: string){
         super(message)
         this.name = "MissingPersonalSkillIdError"
+
+        Object.setPrototypeOf(this, MissingPersonalSkillIdError.prototype)
     }
 }
