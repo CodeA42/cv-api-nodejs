42f8ad258cb10488ac1609f6eb63e5f484958aff
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 10:56:39 +0300

    should work better

--- lib/utils/middlewares/userExists.ts
+++ lib/utils/middlewares/userExists.ts
@@ -1,11 +1,13 @@
-import { NextFunction, Request, Response } from "express";
-import getUserById from "../../db/queries/user/get";
+import { NextFunction, Request, Response } from "express"
+import getUserById from "../../db/queries/user/get"
+import MissingUserIdError from "../../error/MissingUserIdError"
 
 export default async function userExists(req: Request, res: Response, next: NextFunction) {
-    const dbRes = await getUserById(res.locals.user.id);
-    
-    if(dbRes.length !== 1){
-        next();
+    try {
+        await getUserById(res.locals.user.id)
+        next()
+    } catch(e) {
+        if(e instanceof MissingUserIdError) return res.status(400).json(e.message)
     }
-    res.sendStatus(409);
+    res.sendStatus(409)
 }
