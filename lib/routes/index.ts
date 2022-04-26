9129657bd23a23e5ad8029c8f6f58b6ed4baff35
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 15:02:08 +0300

    moved authentication to the top of the router

--- lib/routes/index.ts
+++ lib/routes/index.ts
@@ -1,13 +1,16 @@
-import { Router, Request, Response } from "express";
-import cvRouter from "./cv";
-import userRouter from "./user";
+import { Router, Request, Response } from "express"
+import authenticate from "../utils/middlewares/authenticateUser"
+import getAccessToken from "../utils/middlewares/authenticateUser/getAccessToken"
+import cvRouter from "./cv"
+import userRouter from "./user"
 
-const router: Router = Router();  
-router.use('/user', userRouter);
-router.use('/cv', cvRouter);
+const router: Router = Router()
+router.use(authenticate(getAccessToken))
+router.use('/user', userRouter)
+router.use('/cv', cvRouter)
 
 router.use("*", (req: Request, res: Response) => {
-    res.status(404).json(`Invalid request {${req.originalUrl}}`);
+    res.status(404).json(`Invalid request {${req.originalUrl}}`)
 })
 
-export default router;
+export default router
