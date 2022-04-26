75a5ca86750fef50cae9c5737cebc4ae7a0bc074
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 17:35:41 +0300

    removed the link to the iamge entity

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,12 +1,11 @@
-import { DataSource } from "typeorm";
-import Cv from "./Entities/Cv.Entity";
-import Edication from "./Entities/Education.Entity";
-import Experience from "./Entities/Experience.Entity";
-import Image from "./Entities/Image.Entity";
-import PersonalSkills from "./Entities/PersonalSkills.Entity";
-import User from "./Entities/User.Entity";
-import UserDetails from "./Entities/UserDetails.Entity";
-import WorkExperience from "./Entities/WorkExperience.Entity";
+import { DataSource } from "typeorm"
+import Cv from "./Entities/Cv.Entity"
+import Edication from "./Entities/Education.Entity"
+import Experience from "./Entities/Experience.Entity"
+import PersonalSkills from "./Entities/PersonalSkills.Entity"
+import User from "./Entities/User.Entity"
+import UserDetails from "./Entities/UserDetails.Entity"
+import WorkExperience from "./Entities/WorkExperience.Entity"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -15,8 +14,8 @@ export const AppDataSource = new DataSource({
     username: "cvapiuser",
     password: "supersafepassword",
     database: "cvapi",
-    entities:[User, Cv, Edication, Experience, PersonalSkills, WorkExperience, UserDetails, Image],
+    entities:[User, Cv, Edication, Experience, PersonalSkills, WorkExperience, UserDetails],
     synchronize: true
 });
 
-AppDataSource.initialize().then(() => {console.log(`Db init success`)}).catch((e) => console.error(e));
+AppDataSource.initialize().then(() => {console.log(`Db init success`)}).catch((e) => console.error(e))
