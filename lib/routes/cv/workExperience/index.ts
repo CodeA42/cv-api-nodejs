2d88f893f4dae8fb2615afc1b099277b12c6031b
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 15:01:28 +0300

    moved authentication to the top of the router

--- lib/routes/cv/workExperience/index.ts
+++ lib/routes/cv/workExperience/index.ts
@@ -1,13 +1,11 @@
 import { Request, Response, Router } from "express"
-import authenticate from "../../../utils/middlewares/authenticateUser"
-import getAccessToken from "../../../utils/middlewares/authenticateUser/getAccessToken"
 import authorise from "../../../utils/middlewares/authorise"
 import fromTargetWorkExperienceEntity from "../../../utils/middlewares/authorise/fromTargetWorkExperienceEntity"
 import deleteWorkExperience from "./delete"
 
 const workExpericenceRouter: Router = Router()
 
-workExpericenceRouter.delete('/:id', authenticate(getAccessToken), authorise(fromTargetWorkExperienceEntity), deleteWorkExperience)
+workExpericenceRouter.delete('/:id', authorise(fromTargetWorkExperienceEntity), deleteWorkExperience)
 
 workExpericenceRouter.use("*", (req: Request, res: Response) => {
     res.status(404).json(`Invalid request {${req.originalUrl}}`)
