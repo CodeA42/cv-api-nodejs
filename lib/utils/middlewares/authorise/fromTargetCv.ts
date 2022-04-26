770efc45d3f1dad9f0f8042c7da542eec83f014c
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 17:33:18 +0300

    simplified will probably remove at some point

--- lib/utils/middlewares/authorise/fromTargetCv.ts
+++ lib/utils/middlewares/authorise/fromTargetCv.ts
@@ -1,19 +1,13 @@
 import { Request, Response } from "express"
 import User from "../../../db/Entities/User.Entity"
 import getUserIdFromCvId from "../../../db/queries/user/getUserIdByCvId"
-import CvOrUserNotFoundError from "../../../error/CvOrUserNotFoundError"
 import MissingCvIdError from "../../../error/MissingCvIdError"
 
 export default async function fromTargetCv(req: Request, res: Response){
-    if(!req.params.id) throw new MissingCvIdError(MissingCvIdError.defaultMessage)
-    
-    try {
-        const user: User = await getUserIdFromCvId(req.params.id)
-        if(user) return user.id
-    } catch(e) {
-        if(e instanceof CvOrUserNotFoundError) {
-            return res.status(404).json(e.message)
-        }
-    }
+    const id: string = req.params.id
+    if(!id) throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+
+    const user: User = await getUserIdFromCvId(id)
+    if(user) return user.id
     return null
 }
