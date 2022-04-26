7c48f4a254f4af408baffb59410294038360d32d
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 15:26:26 +0300

    sould probably work duno

--- lib/utils/middlewares/userExists.ts
+++ lib/utils/middlewares/userExists.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, Response } from "express"
+import User from "../../db/Entities/User.Entity"
 import getUserById from "../../db/queries/user/get"
 import MissingUserIdError from "../../error/MissingUserIdError"
+import UserNotFoundError from "../../error/UserNotFoundError"
 
 export default async function userExists(req: Request, res: Response, next: NextFunction) {
     try {
-        await getUserById(res.locals.user.id)
-        next()
+        const user: User = await getUserById(res.locals.user.id)
+        if(user) return res.sendStatus(409)
     } catch(e) {
         if(e instanceof MissingUserIdError) return res.status(400).json(e.message)
+        if(e instanceof UserNotFoundError) return next()
+        console.error(e)
+        return res.sendStatus(500)
     }
-    res.sendStatus(409)
 }
