30ed5c4684bd2753145510613af1a20bc4aac2a1
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 15:00:56 +0300

    moved auth to the the top of the router

--- lib/routes/cv/education/index.ts
+++ lib/routes/cv/education/index.ts
@@ -1,13 +1,11 @@
 import { Request, Response, Router } from "express"
-import authenticate from "../../../utils/middlewares/authenticateUser"
-import getAccessToken from "../../../utils/middlewares/authenticateUser/getAccessToken"
 import authorise from "../../../utils/middlewares/authorise"
 import fromTargetEducationEntity from "../../../utils/middlewares/authorise/fromTargetEducationEntity"
 import deleteEducation from "./delete"
 
 const educationRouter: Router = Router()
 
-educationRouter.delete('/:id', authenticate(getAccessToken), authorise(fromTargetEducationEntity), deleteEducation)
+educationRouter.delete('/:id', authorise(fromTargetEducationEntity), deleteEducation)
 
 educationRouter.use("*", (req: Request, res: Response) => {
     res.status(404).json(`Invalid request {${req.originalUrl}}`)
