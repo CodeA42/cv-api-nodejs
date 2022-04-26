7f0aaf0fda661ab6ab457925e9418102be570ba8
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 14:59:40 +0300

    Prototype should be set manually in ts

--- lib/error/CvNotFoundError.ts
+++ lib/error/CvNotFoundError.ts
@@ -3,5 +3,7 @@ export default class CvNotFoundError extends Error {
     constructor(message: string){
         super(message)
         this.name = "CvNotFoundError"
+
+        Object.setPrototypeOf(this, CvNotFoundError.prototype)
     }
 }
