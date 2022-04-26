cb7c5c8ef13e1dca864ecb69720eb1367926ba55
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-26 15:01:42 +0300

    moved authentication to the top of the router

--- lib/routes/user/index.ts
+++ lib/routes/user/index.ts
@@ -1,16 +1,15 @@
-import { Router, Request, Response } from "express";
-import getAccessToken from "../../utils/middlewares/authenticateUser/getAccessToken";
-import firstTime from "./firstTime";
-import updateUser from "./update";
-import userExists from "../../utils/middlewares/userExists";
-import multer from "../../utils/middlewares/multer";
-import prepUserData from "../../utils/middlewares/prepUserData";
-import authenticate from "../../utils/middlewares/authenticateUser";
+import { Router, Request, Response } from "express"
+import firstTime from "./firstTime"
+import updateUser from "./update"
+import userExists from "../../utils/middlewares/userExists"
+import multer from "../../utils/middlewares/multer"
+import prepUserData from "../../utils/middlewares/prepUserData"
+import authenticate from "../../utils/middlewares/authenticateUser"
 
 
 const userRouter: Router = Router();  
-userRouter.post('/first-time', authenticate(getAccessToken), userExists, firstTime);
-userRouter.put('/', authenticate(getAccessToken), multer.array('avatar'), prepUserData , updateUser);
+userRouter.post('/first-time', userExists, firstTime);
+userRouter.put('/', multer.array('avatar'), prepUserData , updateUser);
 
 userRouter.use("*", (req: Request, res: Response) => {
     res.status(404).json(`Invalid request {${req.originalUrl}}`);
