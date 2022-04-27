5e3f3cf0354785cf616f3ad3b5ee620e2ad129d7
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 16:19:33 +0300

    moved the default message in the consturcor

--- lib/error/MissingPersonalSkillIdError.ts
+++ lib/error/MissingPersonalSkillIdError.ts
@@ -1,6 +1,5 @@
 export default class MissingPersonalSkillIdError extends Error {
-    static defaultMessage: string = "Missing personal skill id"
-    constructor(message: string){
+    constructor(message: string = "Missing personal skill id"){
         super(message)
         this.name = "MissingPersonalSkillIdError"
     }
