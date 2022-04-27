65e250b029bef12d3681f1043bf1b3984a97b183
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 11:28:16 +0300

    image in db...

--- lib/db/queries/cv/image/deleteImageEntity.ts
+++
@@ -1,9 +0,0 @@
-import { AppDataSource } from "../../..";
-import MissingImageIdError from "../../../../error/MissingImageIdError";
-import Image from "../../../Entities/Image.Entity";
-
-export default async function deleteImageEntity(id: string) {
-    if(!id) throw new MissingImageIdError(MissingImageIdError.defaultMessage)
-    
-    return await AppDataSource.manager.delete(Image, { id })
-}
