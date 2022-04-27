5dc9534ec6ef18a792e224608c3aaae44169d37a
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 11:33:58 +0300

    reworked should be better now

--- lib/routes/cv/image/put.ts
+++ lib/routes/cv/image/put.ts
@@ -1,13 +1,19 @@
-import { Request, Response } from "express";
-import updateImage from "../../../db/queries/cv/update/updateImage";
+import { Request, Response } from "express"
+import updateImage from "../../../db/queries/cv/update/updateImage"
+import ImageNotSavedError from "../../../error/ImageNotSavedError"
+import MissingCvIdError from "../../../error/MissingCvIdError"
 
 export default async function putImage(req: Request, res: Response) {
     try {
-        await updateImage(req.body.cvId, req.file.buffer.toString("base64"))
+        const image: string = req.file.filename
+        const cvId: string = req.body.id
+        await updateImage(cvId, image)
     } catch(e) {
-        console.error(e);
+        if(e instanceof MissingCvIdError) return res.status(400).json(MissingCvIdError.defaultMessage)
+        if(e instanceof ImageNotSavedError) return res.sendStatus(500)
+        console.error(e)
         res.sendStatus(500)
     }
 
-    res.sendStatus(200);
+    res.sendStatus(200)
 }
