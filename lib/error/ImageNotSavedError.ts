75264fd83767fe6c44225c158bc590e8162b806d
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 16:19:15 +0300

    moved the default message in the consturcor

--- lib/error/ImageNotSavedError.ts
+++ lib/error/ImageNotSavedError.ts
@@ -1,6 +1,5 @@
 export default class ImageNotSavedError extends Error {
-    static defaultMessage: string = "Image not saved"
-    constructor(message: string){
+    constructor(message: string = "Image not saved"){
         super(message)
         this.name = "ImageNotSavedError"
     }
