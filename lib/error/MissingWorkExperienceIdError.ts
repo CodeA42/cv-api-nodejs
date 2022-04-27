158850b3b40b0c761d28c5fe0da92e72e3659e10
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 16:19:29 +0300

    moved the default message in the consturcor

--- lib/error/MissingWorkExperienceIdError.ts
+++ lib/error/MissingWorkExperienceIdError.ts
@@ -1,6 +1,5 @@
 export default class MissingWorkExperienceIdError extends Error {
-    static defaultMessage: string = "Missing work experience id"
-    constructor(message: string){
+    constructor(message: string = "Missing work experience id"){
         super(message)
         this.name = "MissingWorkExperienceIdError"
     }
