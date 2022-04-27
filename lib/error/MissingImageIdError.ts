dd5a807e54fcbbb08ea545473392cd775247584f
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 12:03:27 +0300

    never used

--- lib/error/MissingImageIdError.ts
+++
@@ -1,7 +0,0 @@
-export default class MissingImageIdError extends Error {
-    static defaultMessage: string = "Missing image id"
-    constructor(message: string){
-        super(message)
-        this.name = "MissingImageIdError"
-    }
-}
--- lib/error/PersonalSkillNotFoundError.ts
+++
@@ -1,7 +0,0 @@
-export default class PersonalSkillNotFoundError extends Error {
-    static defaultMessage: string = "Personal skill not found"
-    constructor(message: string){
-        super(message)
-        this.name = "PersonalSkillNotFoundError"
-    }
-}
