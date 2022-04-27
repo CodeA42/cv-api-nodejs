35c3de2abab948241e2dc9d144643036f84a1d7d
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 11:28:58 +0300

    gets name of image with a given cv id

---
+++ lib/db/queries/cv/image/getImageNameWithCvId.ts
@@ -0,0 +1,23 @@
+import { AppDataSource } from "../../.."
+import ImageNotFoundError from "../../../../error/ImageNotFoundError"
+import MissingCvIdError from "../../../../error/MissingCvIdError";
+import MissingImageIdError from "../../../../error/MissingImageIdError"
+import UserDetailsNotFoundError from "../../../../error/UserDetailsNotFoundError";
+import UserDetails from "../../../Entities/UserDetails.Entity";
+import getCvDetailsWithCvId from "../get/getCvDetailsWithCvId";
+
+export default async function getImageNameWithCvId(cvId: string): Promise<string | null> {
+    if(!cvId) throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+    
+    try {
+        const details: UserDetails = await getCvDetailsWithCvId(cvId)
+        if(details.image) return details.image
+    } catch(e) {
+        if(e instanceof UserDetailsNotFoundError) {
+            throw new ImageNotFoundError(ImageNotFoundError.defaultMessage)
+        }
+        console.error(e);
+        return null
+    }
+    throw new ImageNotFoundError(ImageNotFoundError.defaultMessage)
+}
