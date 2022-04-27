55debecbd0f975750e8a3133364e74b9bf7bfa0e
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 11:33:24 +0300

    deletes image properly

--- lib/routes/cv/image/delete.ts
+++ lib/routes/cv/image/delete.ts
@@ -1,15 +1,13 @@
 import { Request, Response } from "express"
-import deleteImageEntity from "../../../db/queries/cv/image/deleteImageEntity"
+import deleteCvImage from "../../../db/queries/cv/image/deleteCvImage"
 import ImageNotFoundError from "../../../error/ImageNotFoundError"
 
 export default async function deleteImage(req: Request, res: Response) {
     try{
-        await deleteImageEntity(req.params.id)
+        await deleteCvImage(req.params.id)
         return res.sendStatus(200)
     } catch(e) {
-        if(e instanceof ImageNotFoundError) {
-            return res.sendStatus(404)
-        }
+        if(e instanceof ImageNotFoundError) return res.sendStatus(404).json(e.message)
         console.error(e)
         return res.sendStatus(500)
     }
