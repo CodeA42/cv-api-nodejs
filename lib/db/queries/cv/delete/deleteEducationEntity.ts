ee16d6584863d62a018118769db8ef9373810aa1
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 16:16:52 +0300

    the default message is in the consturctor

--- lib/db/queries/cv/delete/deleteEducationEntity.ts
+++ lib/db/queries/cv/delete/deleteEducationEntity.ts
@@ -3,7 +3,7 @@ import MissingEducationIdError from "../../../../error/MissingEducationIdError"
 import Education from "../../../Entities/Education.Entity"
 
 export default async function deleteEducationEntity(id: string) {
-    if(!id) throw new MissingEducationIdError(MissingEducationIdError.defaultMessage)
+    if(!id) throw new MissingEducationIdError()
     
     return await AppDataSource.manager.delete(Education, { id })
     
