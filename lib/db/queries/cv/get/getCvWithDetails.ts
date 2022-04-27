257fb6ab489cb16842600beb40a9fe045f98dadd
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 16:17:27 +0300

    the default message is in the consturctor

--- lib/db/queries/cv/get/getCvWithDetails.ts
+++ lib/db/queries/cv/get/getCvWithDetails.ts
@@ -4,7 +4,7 @@ import MissingCvIdError from "../../../../error/MissingCvIdError"
 import Cv from "../../../Entities/Cv.Entity"
 
 export default async function getCvWithDetails(id: string): Promise<Cv | null> {
-    if(!id) throw new MissingCvIdError(MissingCvIdError.defaultMessage)
+    if(!id) throw new MissingCvIdError()
     
     try{
         const cv: Cv = await AppDataSource
@@ -19,5 +19,5 @@ export default async function getCvWithDetails(id: string): Promise<Cv | null> {
         console.error(e);
         return null
     }
-    throw new CvNotFoundError(CvNotFoundError.defaultMessage) 
+    throw new CvNotFoundError() 
 }
