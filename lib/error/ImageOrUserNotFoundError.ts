a01cfae3c32aed10192780629c5ef6611e731083
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 11:57:33 +0300

    no need for that one anymore

--- lib/error/ImageOrUserNotFoundError.ts
+++
@@ -1,7 +0,0 @@
-export default class ImageOrUserNotFoundError extends Error {
-    static defaultMessage: string = "Image or user not found"
-    constructor(message: string){
-        super(message)
-        this.name = "ImageOrUserNotFoundError"
-    }
-}
