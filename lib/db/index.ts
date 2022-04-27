8e69ae69d93d539b3fd8c473c238f4f097d93458
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 14:57:48 +0300

    getting the connection from db.connection.json

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -6,14 +6,15 @@ import PersonalSkills from "./Entities/PersonalSkills.Entity"
 import User from "./Entities/User.Entity"
 import UserDetails from "./Entities/UserDetails.Entity"
 import WorkExperience from "./Entities/WorkExperience.Entity"
+import * as connection from "../config/db.connection.json"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "cvapiuser",
-    password: "supersafepassword",
-    database: "cvapi",
+    host: connection.host,
+    port: connection.port,
+    username: connection.username,
+    password: connection.password,
+    database: connection.database,
     entities:[User, Cv, Edication, Experience, PersonalSkills, WorkExperience, UserDetails],
     synchronize: true
 });
