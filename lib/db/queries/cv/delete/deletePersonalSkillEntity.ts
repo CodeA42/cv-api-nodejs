ab67ac5e91f3179c734fbf3076cebdcd3fe349e9
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 16:16:56 +0300

    the default message is in the consturctor

--- lib/db/queries/cv/delete/deletePersonalSkillEntity.ts
+++ lib/db/queries/cv/delete/deletePersonalSkillEntity.ts
@@ -3,7 +3,7 @@ import MissingPersonalSkillIdError from "../../../../error/MissingPersonalSkillI
 import PersonalSkills from "../../../Entities/PersonalSkills.Entity"
 
 export default async function deletePersonalSkillEntity(id: string) {
-    if(!id) throw new MissingPersonalSkillIdError(MissingPersonalSkillIdError.defaultMessage)
+    if(!id) throw new MissingPersonalSkillIdError()
     
     return await AppDataSource.manager.delete(PersonalSkills, { id })
 }
