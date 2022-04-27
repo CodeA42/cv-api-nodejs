a659a7d8880793c60f27c0862025b7c78d221a2d
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-27 16:17:47 +0300

    the default message is in the consturctor

--- lib/db/queries/cv/create.ts
+++ lib/db/queries/cv/create.ts
@@ -3,7 +3,7 @@ import CvNotFoundError from "../../../error/CvNotFoundError"
 import Cv from "../../Entities/Cv.Entity"
 
 export default async function createNewCv(cv: Cv): Promise<Cv | null>{
-    if(!cv) throw new CvNotFoundError(CvNotFoundError.defaultMessage)
+    if(!cv) throw new CvNotFoundError()
     
     return await AppDataSource.manager.save(cv)
 }
